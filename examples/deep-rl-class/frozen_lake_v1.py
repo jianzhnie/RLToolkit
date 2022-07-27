68e088b9fb21a5ec801f45d0887a3798e163af40
jianzhnie <jianzhnie@126.com>
2022-07-27 20:08:37 +0800

    Update frozen_lake_v1.py

--- examples/deep-rl-class/frozen_lake_v1.py
+++ examples/deep-rl-class/frozen_lake_v1.py
@@ -1,7 +1,14 @@
+import datetime
+import json
 import random
+from pathlib import Path
 
 import gym
+import imageio
 import numpy as np
+import pickle5 as pickle
+from huggingface_hub import HfApi, Repository
+from huggingface_hub.repocard import metadata_eval_result, metadata_save
 
 
 def initialize_q_table(state_space, action_space):
@@ -102,6 +109,164 @@ def evaluate_agent(env, max_steps, n_eval_episodes, Q, seed):
     return mean_reward, std_reward
 
 
+def record_video(env, Qtable, out_directory, fps=1):
+    images = []
+    done = False
+    state = env.reset(seed=random.randint(0, 500))
+    img = env.render(mode='rgb_array')
+    images.append(img)
+    while not done:
+        # Take the action (index) that have the maximum expected future reward given that state
+        action = np.argmax(Qtable[state][:])
+        state, reward, done, info = env.step(
+            action)  # We directly put next_state = state for recording logic
+        img = env.render(mode='rgb_array')
+        images.append(img)
+
+    imageio.mimsave(
+        out_directory, [np.array(img) for i, img in enumerate(images)],
+        fps=fps)
+
+
+def push_to_hub(repo_name,
+                model,
+                env,
+                video_fps=1,
+                local_repo_path='hub',
+                commit_message='Push Q-Learning agent to Hub',
+                token=None):
+
+    eval_env = env
+
+    # Step 1: Clone or create the repo
+    # Create the repo (or clone its content if it's nonempty)
+    api = HfApi()
+
+    repo_url = api.create_repo(
+        name=repo_name,
+        token=token,
+        private=False,
+        exist_ok=True,
+    )
+
+    # Git pull
+    repo_local_path = Path(local_repo_path) / repo_name
+    repo = Repository(
+        repo_local_path, clone_from=repo_url, use_auth_token=True)
+    repo.git_pull()
+
+    repo.lfs_track(['*.mp4'])
+
+    # Step 1: Save the model
+    if env.spec.kwargs.get('map_name'):
+        model['map_name'] = env.spec.kwargs.get('map_name')
+        if env.spec.kwargs.get('is_slippery', '') is False:
+            model['slippery'] = False
+
+    print(model)
+
+    # Pickle the model
+    with open(Path(repo_local_path) / 'q-learning.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+    # Step 2: Evaluate the model and build JSON
+    mean_reward, std_reward = evaluate_agent(eval_env, model['max_steps'],
+                                             model['n_eval_episodes'],
+                                             model['qtable'],
+                                             model['eval_seed'])
+
+    # First get datetime
+    eval_datetime = datetime.datetime.now()
+    eval_form_datetime = eval_datetime.isoformat()
+
+    evaluate_data = {
+        'env_id': model['env_id'],
+        'mean_reward': mean_reward,
+        'n_eval_episodes': model['n_eval_episodes'],
+        'eval_datetime': eval_form_datetime,
+    }
+    # Write a JSON file
+    with open(Path(repo_local_path) / 'results.json', 'w') as outfile:
+        json.dump(evaluate_data, outfile)
+
+    # Step 3: Create the model card
+    # Env id
+    env_name = model['env_id']
+    if env.spec.kwargs.get('map_name'):
+        env_name += '-' + env.spec.kwargs.get('map_name')
+
+    if env.spec.kwargs.get('is_slippery', '') is False:
+        env_name += '-' + 'no_slippery'
+
+    metadata = {}
+    metadata['tags'] = [
+        env_name, 'q-learning', 'reinforcement-learning',
+        'custom-implementation'
+    ]
+
+    # Add metrics
+    eval = metadata_eval_result(
+        model_pretty_name=repo_name,
+        task_pretty_name='reinforcement-learning',
+        task_id='reinforcement-learning',
+        metrics_pretty_name='mean_reward',
+        metrics_id='mean_reward',
+        metrics_value=f'{mean_reward:.2f} +/- {std_reward:.2f}',
+        dataset_pretty_name=env_name,
+        dataset_id=env_name,
+    )
+
+    # Merges both dictionaries
+    metadata = {**metadata, **eval}
+
+    model_card = f"""
+    # **Q-Learning** Agent playing **{env_id}**
+    This is a trained model of a **Q-Learning** agent playing **{env_id}** .
+    """
+
+    model_card += """
+    ## Usage
+    ```python
+    """
+    model_card += f"""model = load_from_hub(repo_id="{repo_name}", filename="q-learning.pkl")
+
+    # Don't forget to check if you need to add additional attributes (is_slippery=False etc)
+    env = gym.make(model["env_id"])
+
+    evaluate_agent(env, model["max_steps"], model["n_eval_episodes"], model["qtable"], model["eval_seed"])
+    """
+
+    model_card += """
+    ```
+    """
+
+    readme_path = repo_local_path / 'README.md'
+    readme = ''
+    if readme_path.exists():
+        with readme_path.open('r', encoding='utf8') as f:
+            readme = f.read()
+    else:
+        readme = model_card
+
+    with readme_path.open('w', encoding='utf-8') as f:
+        f.write(readme)
+
+    # Save our metrics to Readme metadata
+    metadata_save(readme_path, metadata)
+
+    # Step 4: Record a video
+    video_path = repo_local_path / 'replay.mp4'
+    record_video(env, model['qtable'], video_path, video_fps)
+
+    # Push everything to hub
+    print(f'Pushing repo {repo_name} to the Hugging Face Hub')
+    repo.push_to_hub(commit_message=commit_message)
+
+    print(
+        f'Your model is pushed to the hub. You can view your model here: {repo_url}'
+    )
+
+
 if __name__ == '__main__':
     env = gym.make('FrozenLake-v1', map_name='4x4', is_slippery=False)
 
@@ -151,3 +316,24 @@ if __name__ == '__main__':
     mean_reward, std_reward = evaluate_agent(env, max_steps, n_eval_episodes,
                                              Qtable_frozenlake, eval_seed)
     print(f'Mean_reward={mean_reward:.2f} +/- {std_reward:.2f}')
+
+    model = {
+        'env_id': env_id,
+        'max_steps': max_steps,
+        'n_training_episodes': n_training_episodes,
+        'n_eval_episodes': n_eval_episodes,
+        'eval_seed': eval_seed,
+        'learning_rate': learning_rate,
+        'gamma': gamma,
+        'epsilon': epsilon,
+        'max_epsilon': max_epsilon,
+        'min_epsilon': min_epsilon,
+        'decay_rate': decay_rate,
+        'qtable': Qtable_frozenlake
+    }
+
+    from huggingface_hub import notebook_login
+    notebook_login()
+    username = 'jianzhnie'  # FILL THIS
+    repo_name = 'q_FrozenLake_v1_4x4_noSlippery'
+    push_to_hub(repo_id=f'{repo_name}', model=model, env=env)
