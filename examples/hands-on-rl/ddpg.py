acb03c5f582ff94a389ce0e969ba7a23b76cfeae
jianzhnie <jianzhnie@126.com>
2022-08-10 20:05:00 +0800

    Update ddpg.py

--- examples/hands-on-rl/ddpg.py
+++ examples/hands-on-rl/ddpg.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
     device = torch.device(
         'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    env_name = 'Pendulum-v0'
+    env_name = 'Pendulum-v1'
     env = gym.make(env_name)
     random.seed(0)
     np.random.seed(0)
