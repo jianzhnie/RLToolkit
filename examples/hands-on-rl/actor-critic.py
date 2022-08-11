277d119c54e6496e6a1a7766236053bb2861ab74
jianzhnie <jianzhnie@126.com>
2022-08-11 15:24:35 +0800

    Update actor-critic.py

--- examples/hands-on-rl/actor-critic.py
+++ examples/hands-on-rl/actor-critic.py
@@ -6,13 +6,14 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.optim import Adam
 from tqdm import tqdm
 
 sys.path.append('../../')
 from rltoolkit.utils import rl_utils
 
 
-class PolicyNet(torch.nn.Module):
+class PolicyNet(nn.Module):
 
     def __init__(self, state_dim, hidden_dim, action_dim):
         super(PolicyNet, self).__init__()
@@ -23,10 +24,11 @@ class PolicyNet(torch.nn.Module):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        return F.softmax(x, dim=1)
+        x = F.softmax(x, dim=1)
+        return x
 
 
-class ValueNet(torch.nn.Module):
+class ValueNet(nn.Module):
 
     def __init__(self, state_dim, hidden_dim):
         super(ValueNet, self).__init__()
@@ -34,8 +36,10 @@ class ValueNet(torch.nn.Module):
         self.fc2 = nn.Linear(hidden_dim, 1)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
 
 
 class ActorCritic:
@@ -44,12 +48,12 @@ class ActorCritic:
                  gamma, device):
         # 策略网络
         self.actor = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
-        self.critic = ValueNet(state_dim, hidden_dim).to(device)  # 价值网络
+        # 价值网络
+        self.critic = ValueNet(state_dim, hidden_dim).to(device)
         # 策略网络优化器
-        self.actor_optimizer = torch.optim.Adam(
-            self.actor.parameters(), lr=actor_lr)
-        self.critic_optimizer = torch.optim.Adam(
-            self.critic.parameters(), lr=critic_lr)  # 价值网络优化器
+        self.actor_optimizer = Adam(self.actor.parameters(), lr=actor_lr)
+        # 价值网络优化器
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=critic_lr)
         self.gamma = gamma
         self.device = device
 
@@ -77,12 +81,15 @@ class ActorCritic:
         # 时序差分目标
         td_target = rewards + self.gamma * self.critic(next_states) * (1 -
                                                                        dones)
-        td_delta = td_target - self.critic(states)  # 时序差分误差
+        # 时序差分误差
+        td_delta = td_target - self.critic(states)
         log_probs = torch.log(self.actor(states).gather(1, actions))
         actor_loss = torch.mean(-log_probs * td_delta.detach())
+
         # 均方误差损失函数
         critic_loss = torch.mean(
             F.mse_loss(self.critic(states), td_target.detach()))
+
         self.actor_optimizer.zero_grad()
         self.critic_optimizer.zero_grad()
         actor_loss.backward()  # 计算策略网络的梯度
