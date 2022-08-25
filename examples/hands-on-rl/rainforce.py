86405d22a2529c3cc76a9d9f92c53cc86e73f631
jianzhnie <jianzhnie@126.com>
2022-08-25 09:08:47 +0800

    Update rainforce.py

--- examples/hands-on-rl/rainforce.py
+++ examples/hands-on-rl/rainforce.py
@@ -12,18 +12,20 @@ sys.path.append('../../')
 from rltoolkit.utils import rl_utils
 
 
-class PolicyNet(torch.nn.Module):
+class PolicyNet(nn.Module):
 
-    def __init__(self, state_dim, hidden_dim, action_dim):
+    def __init__(self, state_dim, hidden_dim, action_dim, dropout=0.6):
         super(PolicyNet, self).__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.dropout = nn.Dropout(p=dropout)
         self.fc2 = nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        return F.softmax(x, dim=1)
+        x = F.softmax(x, dim=1)
+        return x
 
 
 class REINFORCE:
@@ -42,7 +44,8 @@ class REINFORCE:
         probs = self.policy_net(state)
         action_dist = torch.distributions.Categorical(probs)
         action = action_dist.sample()
-        return action.item()
+        log_probs = action_dist.log_prob(action)
+        return action.item(), log_probs
 
     def update(self, transition_dict):
         reward_list = transition_dict['rewards']
@@ -62,15 +65,38 @@ class REINFORCE:
             loss.backward()  # 反向传播计算梯度
         self.optimizer.step()  # 梯度下降
 
+    def update_(self, transition_dict):
+        reward_list = transition_dict['rewards']
+        log_prob_list = transition_dict['log_probs']
+
+        G = 0
+        returns = []
+        policy_loss = []
+        for i in reversed(range(len(reward_list))):  # 从最后一步算起
+            reward = reward_list[i]
+            G = self.gamma * G + reward
+            returns.insert(0, G)
+
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + eps)
+        for log_prob, G in zip(log_prob_list, returns):
+            policy_loss.append(-log_prob * G)
+
+        self.optimizer.zero_grad()
+        loss = torch.cat(policy_loss).sum()
+        loss.backward()  # 反向传播计算梯度
+        self.optimizer.step()  # 梯度下降
+
 
 if __name__ == '__main__':
     learning_rate = 1e-3
-    num_episodes = 1000
+    num_episodes = 80
     hidden_dim = 128
     gamma = 0.98
     device = torch.device(
         'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+    eps = np.finfo(np.float32).eps.item()
     env_name = 'CartPole-v0'
     env = gym.make(env_name)
     env.seed(0)
@@ -81,27 +107,28 @@ if __name__ == '__main__':
                       device)
 
     return_list = []
-    for i in range(10):
-        with tqdm(
-                total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
-            for i_episode in range(int(num_episodes / 10)):
+    for i in range(20):
+        with tqdm(int(num_episodes), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes)):
                 episode_return = 0
                 transition_dict = {
                     'states': [],
                     'actions': [],
                     'next_states': [],
                     'rewards': [],
+                    'log_probs': [],
                     'dones': []
                 }
                 state = env.reset()
                 done = False
                 while not done:
-                    action = agent.take_action(state)
+                    action, log_probs = agent.take_action(state)
                     next_state, reward, done, _ = env.step(action)
                     transition_dict['states'].append(state)
                     transition_dict['actions'].append(action)
                     transition_dict['next_states'].append(next_state)
                     transition_dict['rewards'].append(reward)
+                    transition_dict['log_probs'].append(log_probs)
                     transition_dict['dones'].append(done)
                     state = next_state
                     episode_return += reward
@@ -110,7 +137,7 @@ if __name__ == '__main__':
                 if (i_episode + 1) % 10 == 0:
                     pbar.set_postfix({
                         'episode':
-                        '%d' % (num_episodes / 10 * i + i_episode + 1),
+                        '%d' % (num_episodes * i + i_episode + 1),
                         'return':
                         '%.3f' % np.mean(return_list[-10:])
                     })
