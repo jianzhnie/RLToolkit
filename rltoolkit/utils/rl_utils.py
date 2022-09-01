c57afb0a211f4d1936208e01c90aa0f623cab69a
jianzhnie <jianzhnie@126.com>
2022-09-01 09:38:36 +0800

    Update rl_utils.py

--- rltoolkit/utils/rl_utils.py
+++ rltoolkit/utils/rl_utils.py
@@ -1,26 +1,6 @@
-import collections
-import random
-
 import numpy as np
 import torch
 from tqdm import tqdm
-
-
-class ReplayBuffer:
-
-    def __init__(self, capacity):
-        self.buffer = collections.deque(maxlen=capacity)
-
-    def add(self, state, action, reward, next_state, done):
-        self.buffer.append((state, action, reward, next_state, done))
-
-    def sample(self, batch_size):
-        transitions = random.sample(self.buffer, batch_size)
-        state, action, reward, next_state, done = zip(*transitions)
-        return np.array(state), action, reward, np.array(next_state), done
-
-    def size(self):
-        return len(self.buffer)
 
 
 def moving_average(a, window_size):
