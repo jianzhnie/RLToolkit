bd180848c2b1aaf640ea56f93db0f08c8cd4b15a
jianzhnie <jianzhnie@126.com>
2022-09-01 15:08:17 +0800

    Update utils.py

--- rltoolkit/utils/utils.py
+++ rltoolkit/utils/utils.py
@@ -1,3 +1,12 @@
+'''
+Author: jianzhnie@126.com
+Date: 2022-08-31 17:25:32
+LastEditors: jianzhnie@126.com
+LastEditTime: 2022-09-01 11:30:28
+Description:
+
+Copyright (c) 2022 by jianzhnie jianzhnie@126.com, All Rights Reserved.
+'''
 import glob
 import os
 
@@ -45,3 +54,32 @@ def cleanup_log_dir(log_dir):
         files = glob.glob(os.path.join(log_dir, '*.monitor.csv'))
         for f in files:
             os.remove(f)
+
+
+def check_model_method(model, method, algo):
+    """check method existence for input model to algo.
+
+    Args:
+        model(parl.Model): model for checking
+        method(str): method name
+        algo(str): algorithm name
+
+    Raises:
+        AssertionError: if method is not implemented in model
+    """
+    if method == 'forward':
+        # check if forward is overridden by the subclass
+        assert callable(
+            getattr(model, 'forward',
+                    None)), 'forward should be a function in model class'
+        assert model.forward.__func__ is not super(
+            model.__class__, model
+        ).forward.__func__, "{}'s model needs to implement forward method. \n".format(
+            algo)
+    else:
+        # check if the specified method is implemented
+        assert hasattr(model, method) and callable(
+            getattr(
+                model, method,
+                None)), "{}'s model needs to implement {} method. \n".format(
+                    algo, method)
