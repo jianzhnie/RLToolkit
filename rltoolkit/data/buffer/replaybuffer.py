c0b48e80ff1385c642d4a2771ca5f5837056d6b1
jianzhnie <jianzhnie@126.com>
2022-09-01 09:37:51 +0800

    Create replaybuffer.py

---
+++ rltoolkit/data/buffer/replaybuffer.py
@@ -0,0 +1,21 @@
+import collections
+import random
+
+import numpy as np
+
+
+class ReplayBuffer:
+
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        transitions = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = zip(*transitions)
+        return np.array(state), action, reward, np.array(next_state), done
+
+    def size(self):
+        return len(self.buffer)
