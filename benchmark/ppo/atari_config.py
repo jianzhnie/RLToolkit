0c5c5f5df53fc3afff88d5c137908dd6d4f1b9a5
jianzhnie <jianzhnie@126.com>
2022-09-02 10:55:32 +0800

    Update atari_config.py

--- benchmark/ppo/atari_config.py
+++ benchmark/ppo/atari_config.py
@@ -2,7 +2,7 @@
 Author: jianzhnie
 Date: 2022-09-01 15:34:45
 LastEditors: jianzhnie
-LastEditTime: 2022-09-01 15:39:30
+LastEditTime: 2022-09-02 10:14:28
 Description:
 Copyright (c) 2022 by jianzhnie@126.com, All Rights Reserved.
 '''
@@ -27,14 +27,15 @@ atari_config = {
     # ========== coefficient of ppo ==========
     'initial_lr': 2.5e-4,  # start learning rate
     'lr_decay': True,  # whether or not to use linear decay rl
-    # 'eps': 1e-5,  # Adam optimizer epsilon (default: 1e-5)
+    'eps': 1e-5,  # Adam optimizer epsilon (default: 1e-5)
     'clip_param': 0.1,  # epsilon in clipping loss
     'entropy_coef': 0.01,  # Entropy coefficient (ie. c_2 in the paper)
-    # 'value_loss_coef': 0.5,  # Value loss coefficient (ie. c_1 in the paper)
-    # 'max_grad_norm': 0.5,  # Max gradient norm for gradient clipping
-    # 'use_clipped_value_loss': True,  # advantages normalization
-    # 'clip_vloss': True,  # whether or not to use a clipped loss for the value function
-    # 'gamma': 0.99, # discounting factor
-    # 'gae': True,  # whether or not to use GAE
-    # 'gae_lambda': 0.95,  # Lambda parameter for calculating N-step advantage
+    'value_loss_coef': 0.5,  # Value loss coefficient (ie. c_1 in the paper)
+    'max_grad_norm': 0.5,  # Max gradient norm for gradient clipping
+    'use_clipped_value_loss': True,  # advantages normalization
+    'clip_vloss': True,
+    # whether or not to use a clipped loss for the value function
+    'gamma': 0.99,  # discounting factor
+    'gae': True,  # whether or not to use GAE
+    'gae_lambda': 0.95,  # Lambda parameter for calculating N-step advantage
 }
