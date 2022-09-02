3de4002459a37336c1da3b0215fac679d7ceb65d
jianzhnie <jianzhnie@126.com>
2022-09-02 09:09:19 +0800

    Update ppov2.py

--- rltoolkit/policy/modelfree/ppov2.py
+++ rltoolkit/policy/modelfree/ppov2.py
@@ -1,8 +1,8 @@
 '''
 Author: jianzhnie@126.com
 Date: 2022-09-01 12:33:42
-LastEditors: jianzhnie@126.com
-LastEditTime: 2022-09-01 14:20:52
+LastEditors: jianzhnie
+LastEditTime: 2022-09-02 09:00:12
 Description:
 
 Copyright (c) 2022 by jianzhnie jianzhnie@126.com, All Rights Reserved.
@@ -21,7 +21,7 @@ class PPO(Algorithm):
 
     def __init__(self,
                  model=None,
-                 clip_param=0.2,
+                 clip_param=0.1,
                  value_loss_coef=0.5,
                  entropy_coef=0.01,
                  initial_lr=2.5e-4,
@@ -32,6 +32,20 @@ class PPO(Algorithm):
                  continuous_action=False,
                  device='cpu'):
         super().__init__(model)
+        """ PPO algorithm
+
+        Args:
+            model (parl.Model): forward network of actor and critic.
+            clip_param (float): epsilon in clipping loss.
+            value_loss_coef (float): value function loss coefficient in the optimization objective.
+            entropy_coef (float): policy entropy coefficient in the optimization objective.
+            initial_lr (float): learning rate.
+            eps (float): Adam optimizer epsilon.
+            max_grad_norm (float): max gradient norm for gradient clipping.
+            use_clipped_value_loss (bool): whether or not to use a clipped loss for the value function.
+            norm_adv (bool): whether or not to use advantages normalization.
+            continuous_action (bool): whether or not is continuous action environment.
+        """
 
         self.clip_param = clip_param
         self.value_loss_coef = value_loss_coef
@@ -57,14 +71,35 @@ class PPO(Algorithm):
               batch_logprob,
               batch_adv,
               lr=None):
+        """update model with PPO algorithm.
 
+        Args:
+            batch_obs (torch.Tensor):           shape([batch_size] + obs_shape)
+            batch_action (torch.Tensor):        shape([batch_size] + action_shape)
+            batch_value (torch.Tensor):         shape([batch_size])
+            batch_return (torch.Tensor):        shape([batch_size])
+            batch_logprob (torch.Tensor):       shape([batch_size])
+            batch_adv (torch.Tensor):           shape([batch_size])
+            lr (torch.Tensor):
+        Returns:
+            value_loss (float): value loss
+            action_loss (float): policy loss
+            entropy_loss (float): entropy loss
+        """
         values = self.model.value(batch_obs)
-        logits = self.model.policy(batch_obs)
-        dist = Categorical(logits=logits)
-        action_log_probs = dist.log_prob(batch_action).sum(1)
-        dist_entropy = dist.entropy()
+        if self.continuous_action:
+            mean, std = self.model.policy(batch_obs)
+            dist = Normal(mean, std)
+            action_log_probs = dist.log_prob(batch_action).sum(1)
+            dist_entropy = dist.entropy().sum(1)
+        else:
+            logits = self.model.policy(batch_obs)
+            dist = Categorical(logits=logits)
+            action_log_probs = dist.log_prob(batch_action)
+            dist_entropy = dist.entropy()
 
         entropy_loss = dist_entropy.mean()
+
         if self.norm_adv:
             batch_adv = (batch_adv - batch_adv.mean()) / (
                 batch_adv.std() + 1e-8)
@@ -75,15 +110,15 @@ class PPO(Algorithm):
                             1.0 + self.clip_param) * batch_adv
 
         action_loss = -torch.min(surr1, surr2).mean()
+
         values = values.view(-1)
         if self.use_clipped_value_loss:
             value_pred_clipped = batch_value + torch.clamp(
                 values - batch_value, -self.clip_param, self.clip_param)
-
-            value_loss = (values - batch_return).pow(2)
+            value_losses = (values - batch_return).pow(2)
             value_losses_clipped = (value_pred_clipped - batch_return).pow(2)
-
-            value_loss = 0.5 * torch.max(value_loss, value_losses_clipped)
+            value_loss = 0.5 * torch.max(value_losses,
+                                         value_losses_clipped).mean()
         else:
             value_loss = 0.5 * (batch_return - values).pow(2).mean()
 
@@ -97,6 +132,7 @@ class PPO(Algorithm):
         loss.backward()
         nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
         self.optimizer.step()
+
         return value_loss.item(), action_loss.item(), entropy_loss.item()
 
     def sample(self, obs):
