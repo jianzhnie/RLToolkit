48b08a88c97cada7bbd14d91717264567c28bd45
jianzhnie <jianzhnie@126.com>
2022-09-02 19:36:01 +0800

    Update train.py

--- benchmark/coma/train.py
+++ benchmark/coma/train.py
@@ -1,17 +1,11 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+'''
+Author: jianzhnie
+Date: 2022-09-02 19:35:02
+LastEditors: jianzhnie
+LastEditTime: 2022-09-02 19:35:35
+Description:
+Copyright (c) 2022 by jianzhnie@126.com, All Rights Reserved.
+'''
 import os
 
 import numpy as np
