f16cd4f993826d3fea954db0a8058779543b419c
jianzhnie <jianzhnie@126.com>
2022-09-03 20:33:59 +0800

    Create ddpg.py

---
+++ rltoolkit/policy/modelfree/ddpg.py
@@ -0,0 +1,88 @@
+from copy import deepcopy
+
+import torch
+import torch.nn.functional as F
+
+from rltoolkit.policy.base_policy import Algorithm
+from rltoolkit.utils.utils import check_model_method
+
+__all__ = ['DDPG']
+
+
+class DDPG(Algorithm):
+
+    def __init__(self,
+                 model,
+                 gamma=None,
+                 tau=None,
+                 actor_lr=None,
+                 critic_lr=None,
+                 device='cpu'):
+        # checks
+        check_model_method(model, 'value', self.__class__.__name__)
+        check_model_method(model, 'policy', self.__class__.__name__)
+        check_model_method(model, 'get_actor_params', self.__class__.__name__)
+        check_model_method(model, 'get_critic_params', self.__class__.__name__)
+        assert isinstance(gamma, float)
+        assert isinstance(tau, float)
+        assert isinstance(actor_lr, float)
+        assert isinstance(critic_lr, float)
+
+        self.gamma = gamma
+        self.tau = tau
+        self.actor_lr = actor_lr
+        self.critic_lr = critic_lr
+
+        self.model = model.to(device)
+        self.target_model = deepcopy(self.model)
+        self.actor_optimizer = torch.optim.Adam(
+            self.model.get_actor_params(), lr=actor_lr)
+        self.critic_optimizer = torch.optim.Adam(
+            self.model.get_critic_params(), lr=critic_lr)
+
+    def predict(self, obs):
+        return self.model.policy(obs)
+
+    def learn(self, obs, action, reward, next_obs, terminal):
+        critic_loss = self._critic_learn(obs, action, reward, next_obs,
+                                         terminal)
+        actor_loss = self._actor_learn(obs)
+
+        self.sync_target()
+        return critic_loss, actor_loss
+
+    def _critic_learn(self, obs, action, reward, next_obs, terminal):
+        # Compute the target Q value
+        target_Q = self.target_model.value(next_obs,
+                                           self.target_model.policy(next_obs))
+        target_Q = reward + ((1. - terminal) * self.gamma * target_Q).detach()
+
+        # Get current Q estimate
+        current_Q = self.model.value(obs, action)
+
+        # Compute critic loss
+        critic_loss = F.mse_loss(current_Q, target_Q)
+
+        # Optimize the critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+        return critic_loss
+
+    def _actor_learn(self, obs):
+        # Compute actor loss and Update the frozen target models
+        actor_loss = -self.model.value(obs, self.model.policy(obs)).mean()
+
+        # Optimize the actor
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+        return actor_loss
+
+    def sync_target(self, decay=None):
+        if decay is None:
+            decay = 1.0 - self.tau
+        for param, target_param in zip(self.model.parameters(),
+                                       self.target_model.parameters()):
+            target_param.data.copy_((1 - decay) * param.data +
+                                    decay * target_param.data)
