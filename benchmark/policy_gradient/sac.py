9743688091a05ad07240cede3ce308f4f52c9d5c
jianzhnie <jianzhnie@126.com>
2022-09-06 10:47:43 +0800

    add ddpg sac td3 algo

---
+++ benchmark/policy_gradient/sac.py
@@ -0,0 +1,509 @@
+'''
+Author: jianzhnie
+LastEditors: jianzhnie
+Description:
+Copyright (c) 2022 by jianzhnie@126.com, All Rights Reserved.
+'''
+import random
+from typing import Dict, List, Tuple
+
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
+
+
+class ReplayBuffer:
+    """A simple numpy replay buffer."""
+
+    def __init__(self, obs_dim: int, size: int, batch_size: int = 32):
+        """Initialize."""
+        self.obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
+        self.next_obs_buf = np.zeros([size, obs_dim], dtype=np.float32)
+        self.acts_buf = np.zeros([size], dtype=np.float32)
+        self.rews_buf = np.zeros([size], dtype=np.float32)
+        self.done_buf = np.zeros([size], dtype=np.float32)
+        self.max_size, self.batch_size = size, batch_size
+        self.ptr, self.size, = 0, 0
+
+    def store(
+        self,
+        obs: np.ndarray,
+        act: np.ndarray,
+        rew: float,
+        next_obs: np.ndarray,
+        done: bool,
+    ):
+        """Store the transition in buffer."""
+        self.obs_buf[self.ptr] = obs
+        self.next_obs_buf[self.ptr] = next_obs
+        self.acts_buf[self.ptr] = act
+        self.rews_buf[self.ptr] = rew
+        self.done_buf[self.ptr] = done
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample_batch(self) -> Dict[str, np.ndarray]:
+        """Randomly sample a batch of experiences from memory."""
+        idxs = np.random.choice(self.size, size=self.batch_size, replace=False)
+        return dict(
+            obs=self.obs_buf[idxs],
+            next_obs=self.next_obs_buf[idxs],
+            acts=self.acts_buf[idxs],
+            rews=self.rews_buf[idxs],
+            done=self.done_buf[idxs])
+
+    def __len__(self) -> int:
+        return self.size
+
+
+def init_layer_uniform(layer: nn.Linear, init_w: float = 3e-3) -> nn.Linear:
+    """Init uniform parameters on the single layer."""
+    layer.weight.data.uniform_(-init_w, init_w)
+    layer.bias.data.uniform_(-init_w, init_w)
+
+    return layer
+
+
+class Actor(nn.Module):
+
+    def __init__(
+        self,
+        in_dim: int,
+        out_dim: int,
+        log_std_min: float = -20,
+        log_std_max: float = 2,
+    ):
+        """Initialize."""
+        super(Actor, self).__init__()
+
+        # set the log std range
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+
+        # set the hidden layers
+        self.hidden1 = nn.Linear(in_dim, 128)
+        self.hidden2 = nn.Linear(128, 128)
+
+        # set log_std layer
+        self.log_std_layer = nn.Linear(128, out_dim)
+        self.log_std_layer = init_layer_uniform(self.log_std_layer)
+
+        # set mean layer
+        self.mu_layer = nn.Linear(128, out_dim)
+        self.mu_layer = init_layer_uniform(self.mu_layer)
+
+    def forward(self, state: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
+        x = F.relu(self.hidden1(state))
+        x = F.relu(self.hidden2(x))
+
+        # get mean
+        mu = self.mu_layer(x).tanh()
+
+        # get std
+        log_std = self.log_std_layer(x).tanh()
+        log_std = self.log_std_min + 0.5 * (self.log_std_max -
+                                            self.log_std_min) * (
+                                                log_std + 1)
+        std = torch.exp(log_std)
+
+        # sample actions
+        dist = Normal(mu, std)
+        z = dist.rsample()
+
+        # normalize action and log_prob
+        # see appendix C of [2]
+        action = z.tanh()
+        log_prob = dist.log_prob(z) - torch.log(1 - action.pow(2) + 1e-7)
+        log_prob = log_prob.sum(-1, keepdim=True)
+
+        return action, log_prob
+
+
+class CriticQ(nn.Module):
+
+    def __init__(self, in_dim: int):
+        """Initialize."""
+        super(CriticQ, self).__init__()
+
+        self.hidden1 = nn.Linear(in_dim, 128)
+        self.hidden2 = nn.Linear(128, 128)
+        self.out = nn.Linear(128, 1)
+        self.out = init_layer_uniform(self.out)
+
+    def forward(self, state: torch.Tensor,
+                action: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
+        x = torch.cat((state, action), dim=-1)
+        x = F.relu(self.hidden1(x))
+        x = F.relu(self.hidden2(x))
+        value = self.out(x)
+
+        return value
+
+
+class CriticV(nn.Module):
+
+    def __init__(self, in_dim: int):
+        """Initialize."""
+        super(CriticV, self).__init__()
+
+        self.hidden1 = nn.Linear(in_dim, 128)
+        self.hidden2 = nn.Linear(128, 128)
+        self.out = nn.Linear(128, 1)
+        self.out = init_layer_uniform(self.out)
+
+    def forward(self, state: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
+        x = F.relu(self.hidden1(state))
+        x = F.relu(self.hidden2(x))
+        value = self.out(x)
+
+        return value
+
+
+class SACAgent(object):
+    """SAC agent interacting with environment.
+
+    Attrtibutes:
+        actor (nn.Module): actor model to select actions
+        actor_optimizer (Optimizer): optimizer for training actor
+        vf (nn.Module): critic model to predict state values
+        vf_target (nn.Module): target critic model to predict state values
+        vf_optimizer (Optimizer): optimizer for training vf
+        qf_1 (nn.Module): critic model to predict state-action values
+        qf_2 (nn.Module): critic model to predict state-action values
+        qf_1_optimizer (Optimizer): optimizer for training qf_1
+        qf_2_optimizer (Optimizer): optimizer for training qf_2
+        env (gym.Env): openAI Gym environment
+        memory (ReplayBuffer): replay memory
+        batch_size (int): batch size for sampling
+        gamma (float): discount factor
+        tau (float): parameter for soft target update
+        initial_random_steps (int): initial random action steps
+        policy_update_freq (int): policy update frequency
+        device (torch.device): cpu / gpu
+        target_entropy (int): desired entropy used for the inequality constraint
+        log_alpha (torch.Tensor): weight for entropy
+        alpha_optimizer (Optimizer): optimizer for alpha
+        transition (list): temporory storage for the recent transition
+        total_step (int): total step numbers
+        is_test (bool): flag to show the current mode (train / test)
+    """
+
+    def __init__(
+            self,
+            env: gym.Env,
+            memory_size: int,
+            batch_size: int,
+            gamma: float = 0.99,
+            tau: float = 5e-3,
+            initial_random_steps: int = int(1e4),
+            policy_update_freq: int = 2,
+    ):
+        """Initialize."""
+        obs_dim = env.observation_space.shape[0]
+        action_dim = env.action_space.shape[0]
+
+        self.env = env
+        self.memory = ReplayBuffer(obs_dim, memory_size, batch_size)
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.tau = tau
+        self.initial_random_steps = initial_random_steps
+        self.policy_update_freq = policy_update_freq
+
+        # device: cpu / gpu
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+        print(self.device)
+
+        # automatic entropy tuning
+        self.target_entropy = -np.prod((action_dim, )).item()  # heuristic
+        self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
+        self.alpha_optimizer = optim.Adam([self.log_alpha], lr=3e-4)
+
+        # actor
+        self.actor = Actor(obs_dim, action_dim).to(self.device)
+
+        # v function
+        self.vf = CriticV(obs_dim).to(self.device)
+        self.vf_target = CriticV(obs_dim).to(self.device)
+        self.vf_target.load_state_dict(self.vf.state_dict())
+
+        # q function
+        self.qf_1 = CriticQ(obs_dim + action_dim).to(self.device)
+        self.qf_2 = CriticQ(obs_dim + action_dim).to(self.device)
+
+        # optimizers
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=3e-4)
+        self.vf_optimizer = optim.Adam(self.vf.parameters(), lr=3e-4)
+        self.qf_1_optimizer = optim.Adam(self.qf_1.parameters(), lr=3e-4)
+        self.qf_2_optimizer = optim.Adam(self.qf_2.parameters(), lr=3e-4)
+
+        # transition to store in memory
+        self.transition = list()
+
+        # total steps count
+        self.total_step = 0
+
+        # mode: train / test
+        self.is_test = False
+
+    def select_action(self, state: np.ndarray) -> np.ndarray:
+        """Select an action from the input state."""
+        # if initial random action should be conducted
+        if self.total_step < self.initial_random_steps and not self.is_test:
+            selected_action = self.env.action_space.sample()
+        else:
+            selected_action = self.actor(
+                torch.FloatTensor(state).to(
+                    self.device))[0].detach().cpu().numpy()
+
+        self.transition = [state, selected_action]
+
+        return selected_action
+
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool]:
+        """Take an action and return the response of the env."""
+        next_state, reward, done, _ = self.env.step(action)
+
+        if not self.is_test:
+            self.transition += [reward, next_state, done]
+            self.memory.store(*self.transition)
+
+        return next_state, reward, done
+
+    def update_model(self) -> Tuple[torch.Tensor, ...]:
+        """Update the model by gradient descent."""
+        device = self.device  # for shortening the following lines
+
+        samples = self.memory.sample_batch()
+        state = torch.FloatTensor(samples['obs']).to(device)
+        next_state = torch.FloatTensor(samples['next_obs']).to(device)
+        action = torch.FloatTensor(samples['acts'].reshape(-1, 1)).to(device)
+        reward = torch.FloatTensor(samples['rews'].reshape(-1, 1)).to(device)
+        done = torch.FloatTensor(samples['done'].reshape(-1, 1)).to(device)
+        new_action, log_prob = self.actor(state)
+
+        # train alpha (dual problem)
+        alpha_loss = (-self.log_alpha.exp() *
+                      (log_prob + self.target_entropy).detach()).mean()
+
+        self.alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.alpha_optimizer.step()
+
+        alpha = self.log_alpha.exp()  # used for the actor loss calculation
+
+        # q function loss
+        mask = 1 - done
+        q_1_pred = self.qf_1(state, action)
+        q_2_pred = self.qf_2(state, action)
+        v_target = self.vf_target(next_state)
+        q_target = reward + self.gamma * v_target * mask
+        qf_1_loss = F.mse_loss(q_1_pred, q_target.detach())
+        qf_2_loss = F.mse_loss(q_2_pred, q_target.detach())
+
+        # v function loss
+        v_pred = self.vf(state)
+        q_pred = torch.min(
+            self.qf_1(state, new_action), self.qf_2(state, new_action))
+        v_target = q_pred - alpha * log_prob
+        vf_loss = F.mse_loss(v_pred, v_target.detach())
+
+        if self.total_step % self.policy_update_freq == 0:
+            # actor loss
+            advantage = q_pred - v_pred.detach()
+            actor_loss = (alpha * log_prob - advantage).mean()
+
+            # train actor
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            # target update (vf)
+            self._target_soft_update()
+        else:
+            actor_loss = torch.zeros(1)
+
+        # train Q functions
+        self.qf_1_optimizer.zero_grad()
+        qf_1_loss.backward()
+        self.qf_1_optimizer.step()
+
+        self.qf_2_optimizer.zero_grad()
+        qf_2_loss.backward()
+        self.qf_2_optimizer.step()
+
+        qf_loss = qf_1_loss + qf_2_loss
+
+        # train V function
+        self.vf_optimizer.zero_grad()
+        vf_loss.backward()
+        self.vf_optimizer.step()
+
+        return actor_loss.data, qf_loss.data, vf_loss.data, alpha_loss.data
+
+    def train(self, num_frames: int, plotting_interval: int = 200):
+        """Train the agent."""
+        self.is_test = False
+
+        state = self.env.reset()
+        actor_losses, qf_losses, vf_losses, alpha_losses = [], [], [], []
+        scores = []
+        score = 0
+
+        for self.total_step in range(1, num_frames + 1):
+            action = self.select_action(state)
+            next_state, reward, done = self.step(action)
+
+            state = next_state
+            score += reward
+
+            # if episode ends
+            if done:
+                state = env.reset()
+                scores.append(score)
+                score = 0
+
+            # if training is ready
+            if (len(self.memory) >= self.batch_size
+                    and self.total_step > self.initial_random_steps):
+                losses = self.update_model()
+                actor_losses.append(losses[0])
+                qf_losses.append(losses[1])
+                vf_losses.append(losses[2])
+                alpha_losses.append(losses[3])
+
+            # plotting
+            if self.total_step % plotting_interval == 0:
+                self._plot(self.total_step, scores, actor_losses, qf_losses,
+                           vf_losses, alpha_losses)
+
+        self.env.close()
+
+    def test(self):
+        """Test the agent."""
+        self.is_test = True
+
+        state = self.env.reset()
+        done = False
+        score = 0
+
+        frames = []
+        while not done:
+            frames.append(self.env.render(mode='rgb_array'))
+            action = self.select_action(state)
+            next_state, reward, done = self.step(action)
+
+            state = next_state
+            score += reward
+
+        print('score: ', score)
+        self.env.close()
+
+        return frames
+
+    def _target_soft_update(self):
+        """Soft-update: target = tau*local + (1-tau)*target."""
+        tau = self.tau
+
+        for t_param, l_param in zip(self.vf_target.parameters(),
+                                    self.vf.parameters()):
+            t_param.data.copy_(tau * l_param.data + (1.0 - tau) * t_param.data)
+
+    def _plot(
+        self,
+        frame_idx: int,
+        scores: List[float],
+        actor_losses: List[float],
+        qf_losses: List[float],
+        vf_losses: List[float],
+        alpha_losses: List[float],
+    ):
+        """Plot the training progresses."""
+
+        def subplot(loc: int, title: str, values: List[float]):
+            plt.subplot(loc)
+            plt.title(title)
+            plt.plot(values)
+
+        subplot_params = [
+            (151, f'frame {frame_idx}. score: {np.mean(scores[-10:])}',
+             scores),
+            (152, 'actor_loss', actor_losses),
+            (153, 'qf_loss', qf_losses),
+            (154, 'vf_loss', vf_losses),
+            (155, 'alpha_loss', alpha_losses),
+        ]
+
+        plt.figure(figsize=(30, 5))
+        for loc, title, values in subplot_params:
+            subplot(loc, title, values)
+        plt.show()
+
+
+class ActionNormalizer(gym.ActionWrapper):
+    """Rescale and relocate the actions."""
+
+    def action(self, action: np.ndarray) -> np.ndarray:
+        """Change the range (-1, 1) to (low, high)."""
+        low = self.action_space.low
+        high = self.action_space.high
+
+        scale_factor = (high - low) / 2
+        reloc_factor = high - scale_factor
+
+        action = action * scale_factor + reloc_factor
+        action = np.clip(action, low, high)
+
+        return action
+
+    def reverse_action(self, action: np.ndarray) -> np.ndarray:
+        """Change the range (low, high) to (-1, 1)."""
+        low = self.action_space.low
+        high = self.action_space.high
+
+        scale_factor = (high - low) / 2
+        reloc_factor = high - scale_factor
+
+        action = (action - reloc_factor) / scale_factor
+        action = np.clip(action, -1.0, 1.0)
+
+        return action
+
+
+if __name__ == '__main__':
+
+    if torch.backends.cudnn.enabled:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    seed = 777
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # environment
+    env_id = 'Pendulum-v1'
+    env = gym.make(env_id)
+    env = ActionNormalizer(env)
+
+    # parameters
+    num_frames = 50000
+    memory_size = 100000
+    batch_size = 128
+    initial_random_steps = 10000
+
+    agent = SACAgent(
+        env,
+        memory_size,
+        batch_size,
+        initial_random_steps=initial_random_steps)
+    agent.train(num_frames)
