ebb55110c2e4c5cc6cf603828539f67639dfcf1c
jianzhnie <jianzhnie@126.com>
2022-09-15 12:21:43 +0800

    update base_concept

---
+++ examples/hands-on-rl/base_concept/markov_decision_process.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+np.random.seed(0)
+
+# 定义状态转移概率矩阵P
+P = [
+    [0.9, 0.1, 0.0, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.6, 0.0, 0.4],
+    [0.0, 0.0, 0.0, 0.0, 0.3, 0.7],
+    [0.0, 0.2, 0.3, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P = np.array(P)
+
+rewards = [-1, -2, -2, 10, 1, 0]  # 定义奖励函数
+gamma = 0.5  # 定义折扣因子
+
+
+# 给定一条序列,计算从某个索引（起始状态）开始到序列最后（终止状态）得到的回报
+def compute_return(start_index, chain, gamma):
+    G = 0
+    for i in reversed(range(start_index, len(chain))):
+        G = gamma * G + rewards[chain[i] - 1]
+    return G
+
+
+# 一个状态序列,s1-s2-s3-s6
+chain = [1, 2, 3, 6]
+start_index = 0
+G = compute_return(start_index, chain, gamma)
+print('根据本序列计算得到回报为：%s。' % G)
+
+
+def compute(P, rewards, gamma, states_num):
+    """利用贝尔曼方程的矩阵形式计算解析解,states_num是MRP的状态数."""
+    rewards = np.array(rewards).reshape((-1, 1))  # 将rewards写成列向量形式
+    value = np.dot(
+        np.linalg.inv(np.eye(states_num, states_num) - gamma * P), rewards)
+    return value
+
+
+V = compute(P, rewards, gamma, 6)
+print('MRP中每个状态价值分别为\n', V)
