8908c594e2cd366c5568602f2cc64bd779841c2d
jianzhnie <jianzhnie@126.com>
2022-09-22 09:59:48 +0800

    add ddpg algo

---
+++ examples/tutorials/lesson5/ddpg/agent.py
@@ -0,0 +1,155 @@
+import copy
+from typing import Any, Tuple
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+
+
+class PolicyNet(nn.Module):
+
+    def __init__(self, state_dim: int, hidden_dim: int, action_dim: int,
+                 action_bound: float):
+        super(PolicyNet, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, action_dim)
+        self.relu = nn.ReLU(inplace=True)
+        # action_bound是环境可以接受的动作最大值
+        self.action_bound = action_bound
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        # 连续动作空间，利用 tanh() 函数将特征映射到 [-1, 1],
+        # 然后通过变换，得到 [low, high] 的输出
+        out = torch.tanh(x) * self.action_bound
+        return out
+
+
+class ValueNet(nn.Module):
+
+    def __init__(self, state_dim: int, hidden_dim: int, action_dim: int):
+        super(ValueNet, self).__init__()
+        self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 1)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
+        # 拼接状态和动作
+        cat = torch.cat([x, a], dim=1)
+        x = self.fc1(cat)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+
+class Agent(object):
+    """Agent interacting with environment.
+
+    The “Critic” estimates the value function. This could be the action-value (the Q value) or state-value (the V value).
+
+    The “Actor” updates the policy distribution in the direction suggested by the Critic (such as with policy gradients).
+
+    Atribute:
+        gamma (float): discount factor
+        entropy_weight (float): rate of weighting entropy into the loss function
+        actor (nn.Module): target actor model to select actions
+        critic (nn.Module): critic model to predict state values
+        actor_optimizer (optim.Optimizer) : optimizer of actor
+        critic_optimizer (optim.Optimizer) : optimizer of critic
+        device (torch.device): cpu / gpu
+    """
+
+    def __init__(self,
+                 state_dim: int,
+                 hidden_dim: int,
+                 action_dim: int,
+                 actor_lr: float,
+                 critic_lr: float,
+                 action_bound: float,
+                 sigma: float,
+                 tau: float,
+                 gamma: float,
+                 device: Any = None):
+
+        self.action_dim = action_dim
+        self.gamma = gamma
+        # 高斯噪声的标准差,均值直接设为0
+        self.sigma = sigma
+        # action_bound是环境可以接受的动作最大值
+        self.action_bound = action_bound
+        # 目标网络软更新参数
+        self.tau = tau
+
+        # 策略网络
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim,
+                               action_bound).to(device)
+        # 价值网络
+        self.critic = ValueNet(state_dim, hidden_dim, action_dim).to(device)
+
+        self.target_actor = copy.deepcopy(self.actor)
+        self.target_critic = copy.deepcopy(self.critic)
+
+        # 策略网络优化器
+        self.actor_optimizer = Adam(self.actor.parameters(), lr=actor_lr)
+        # 价值网络优化器
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=critic_lr)
+        # 折扣因子
+        self.device = device
+
+    def sample(self, obs: np.ndarray):
+        obs = torch.from_numpy(obs).float().unsqueeze(0).to(self.device)
+        action = self.actor(obs).item()
+        action += self.sigma * np.random.randn(self.action_dim)
+        return action
+
+    def predict(self, obs):
+        obs = torch.from_numpy(obs).float().unsqueeze(0).to(self.device)
+        select_action = self.actor(obs).cpu().detach().numpy().flatten()
+        return select_action
+
+    def soft_update(self, net, target_net):
+        for param_target, param in zip(target_net.parameters(),
+                                       net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) +
+                                    param.data * self.tau)
+
+    def learn(self, obs: np.ndarray, action: np.ndarray, reward: np.ndarray,
+              next_obs: np.ndarray,
+              terminal: np.ndarray) -> Tuple[float, float]:
+        """Update the model by TD actor-critic."""
+
+        device = self.device  # for shortening the following lines
+        obs = torch.FloatTensor(obs).to(device)
+        next_obs = torch.FloatTensor(next_obs).to(device)
+        actions = torch.LongTensor(action.reshape(-1, 1)).to(device)
+        rewards = torch.FloatTensor(reward.reshape(-1, 1)).to(device)
+        terminal = torch.FloatTensor(terminal.reshape(-1, 1)).to(device)
+
+        next_actions = self.target_actor(next_obs)
+        next_q_values = self.target_critic(next_obs, next_actions)
+
+        # 时序差分目标
+        q_targets = rewards + self.gamma * next_q_values * (1 - terminal)
+        # 均方误差损失函数
+        value_loss = F.mse_loss(self.critic(obs, actions), q_targets)
+        # update value network
+        self.critic_optimizer.zero_grad()
+        value_loss.backward()
+        self.critic_optimizer.step()
+
+        policy_loss = -torch.mean(self.critic(obs, self.actor(obs)))
+        # update policy
+        self.actor_optimizer.zero_grad()
+        policy_loss.backward()
+        self.actor_optimizer.step()
+
+        # 软更新策略网络
+        self.soft_update(self.actor, self.target_actor)
+        # 软更新价值网络
+        self.soft_update(self.critic, self.target_critic)
+
+        return policy_loss.item(), value_loss.item()
---
+++ examples/tutorials/lesson5/ddpg/train.py
@@ -0,0 +1,191 @@
+import argparse
+import sys
+
+import gym
+import numpy as np
+import torch
+from agent import Agent
+from tqdm import tqdm
+
+sys.path.append('../../../../')
+from rltoolkit.data.buffer.replaybuffer import ReplayBuffer
+from rltoolkit.utils import logger, tensorboard
+
+config = {
+    'train_seed': 42,
+    'test_seed': 42,
+    'env': 'Pendulum-v1',
+    'total_episode': 800,  # max training steps
+    'hidden_dim': 128,
+    'total_steps': 10000,  # max training steps
+    'memory_size': 2000,  # Replay buffer size
+    'memory_warmup_size': 500,  # Replay buffer memory_warmup_size
+    'actor_lr': 5e-4,  # start learning rate
+    'critic_lr': 5e-3,  # end learning rate
+    'gamma': 0.98,  # discounting factor
+    'tau': 0.005,  # 软更新参数,
+    'sigma': 0.01,
+    'entropy_weight': 0.01,
+    'batch_size': 64,
+    'eval_render': False,  # do eval render
+    'test_every_steps': 50,  # evaluation freq
+    'video_folder': 'results'
+}
+
+
+# train an episode
+def run_train_episode(agent: Agent, env: gym.Env, rpm: ReplayBuffer,
+                      memory_warmup_size: int):
+    total_reward = 0
+    obs = env.reset()
+    step = 0
+    policy_loss_lst = []
+    value_loss_lst = []
+    while True:
+        step += 1
+        action = agent.sample(obs)
+        next_obs, reward, done, _ = env.step(action)
+        rpm.append(obs, action, reward, next_obs, done)
+        # train model
+        if rpm.size() > memory_warmup_size:
+            # s,a,r,s',done
+            samples = rpm.sample_batch()
+
+            batch_obs = samples['obs']
+            batch_action = samples['action']
+            batch_reward = samples['reward']
+            batch_next_obs = samples['next_obs']
+            batch_terminal = samples['terminal']
+
+            policy_loss, value_loss = agent.learn(batch_obs, batch_action,
+                                                  batch_reward, batch_next_obs,
+                                                  batch_terminal)
+            policy_loss_lst.append(policy_loss)
+            value_loss_lst.append(value_loss)
+
+        total_reward += reward
+        obs = next_obs
+        if done:
+            break
+    return total_reward, step, np.mean(policy_loss_lst), np.mean(
+        value_loss_lst)
+
+
+# 评估 agent, 跑 5 个episode，总reward求平均
+def evaluate(agent: Agent,
+             env: gym.Env,
+             n_eval_episodes: int = 5,
+             render: bool = False,
+             video_folder: str = None):
+
+    if video_folder is not None:
+        env = gym.wrappers.RecordVideo(env, video_folder=video_folder)
+    eval_rewards = []
+    for i in range(n_eval_episodes):
+        obs = env.reset()
+        done = False
+        episode_reward = 0
+        while not done:
+            action = agent.predict(obs)
+            obs, reward, done, _ = env.step(action)
+            episode_reward += reward
+            if render:
+                env.render()
+            if done:
+                env.close()
+        eval_rewards.append(episode_reward)
+    mean_reward = np.mean(eval_rewards)
+    std_reward = np.std(eval_rewards)
+    return mean_reward, std_reward
+
+
+def main():
+    algo_name = 'ddpg'
+    args = argparse.Namespace(**config)
+    env = gym.make(args.env)
+    test_env = gym.make(args.env)
+
+    # set seed
+    torch.manual_seed(args.train_seed)
+    torch.cuda.manual_seed_all(args.train_seed)
+    env.seed(args.train_seed)
+    test_env.seed(args.test_seed)
+
+    device = torch.device(
+        'cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    obs_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    action_bound = env.action_space.high[0]  # 动作最大值
+    rpm = ReplayBuffer(
+        obs_dim=obs_dim, max_size=args.memory_size, batch_size=args.batch_size)
+
+    logger.info('obs_dim {}, action_dim {}'.format(obs_dim, action_dim))
+
+    agent = Agent(
+        state_dim=obs_dim,
+        action_dim=action_dim,
+        hidden_dim=args.hidden_dim,
+        actor_lr=args.actor_lr,
+        critic_lr=args.critic_lr,
+        action_bound=action_bound,
+        sigma=args.sigma,
+        tau=args.tau,
+        gamma=args.gamma,
+        device=device)
+
+    # start training, memory warm up
+    with tqdm(
+            total=args.memory_warmup_size,
+            desc='[Replay Memory Warm Up]') as pbar:
+        while rpm.size() < args.memory_warmup_size:
+            total_reward, steps, _, _ = run_train_episode(
+                agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
+            pbar.update(steps)
+
+    pbar = tqdm(total=args.total_steps, desc='[Training]')
+    cum_steps = 0  # this is the current timestep
+    test_flag = 0
+    while cum_steps < args.total_steps:
+        # start epoch
+        total_reward, steps, policy_loss, value_loss = run_train_episode(
+            agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
+        cum_steps += steps
+
+        pbar.set_description('train')
+        logger.info(
+            'Plicy Loss {:.2f}, Value Loss {:.2f}, Reward Sum {}.'.format(
+                policy_loss, value_loss, total_reward))
+        tensorboard.add_scalar('{}/training_rewards'.format(algo_name),
+                               total_reward, cum_steps)
+        tensorboard.add_scalar('{}/loss'.format(algo_name), policy_loss,
+                               cum_steps)  # mean of total loss
+        tensorboard.add_scalar('{}/loss'.format(algo_name), value_loss,
+                               cum_steps)  # mean of total loss
+        pbar.update(steps)
+
+        # perform evaluation
+        if cum_steps // args.test_every_steps >= test_flag:
+            while cum_steps // args.test_every_steps >= test_flag:
+                test_flag += 1
+            mean_reward, std_reward = evaluate(agent, test_env)
+            pbar.write('testing')
+            logger.info(
+                'Eval_agent done, steps: {}, mean: {}, std: {:.2f}'.format(
+                    cum_steps, mean_reward, std_reward))
+            tensorboard.add_scalar(
+                '{}/mean_validation_rewards'.format(algo_name), mean_reward,
+                cum_steps)
+
+    # render and record video
+    mean_reward, std_reward = evaluate(
+        agent,
+        test_env,
+        n_eval_episodes=1,
+        render=True,
+        video_folder=args.video_folder)
+    pbar.close()
+
+
+if __name__ == '__main__':
+    main()
