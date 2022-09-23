8f8622dea03fd4f2a22541c3c4e83d6dbda8998b
jianzhnie <jianzhnie@126.com>
2022-09-23 18:05:51 +0800

    Update agent.py

--- examples/tutorials/lesson5/sac_continuous/agent.py
+++ examples/tutorials/lesson5/sac_continuous/agent.py
@@ -6,7 +6,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Categorical
 from torch.distributions.normal import Normal
 from torch.optim import Adam
 
@@ -58,9 +57,7 @@ class PolicyNet(nn.Module):
 
         # sample actions
         dist = Normal(mu, std)
-        action = dist.sample()
         z = dist.rsample()
-
         # normalize action and log_prob
         # see appendix C of [2]
         action = z.tanh()
@@ -144,11 +141,12 @@ class Agent(object):
         # 价值网络优化器
         self.critic1_optimizer = Adam(self.critic1.parameters(), lr=critic_lr)
         self.critic2_optimizer = Adam(self.critic2.parameters(), lr=critic_lr)
+
         # 使用alpha的log值,可以使训练结果比较稳定
-        self.log_alpha = torch.tensor(
-            np.log(0.01), dtype=torch.float, device=device)
-        self.log_alpha.requires_grad = True  # 可以对alpha求梯度
-        self.log_alpha_optimizer = Adam([self.log_alpha], lr=alpha_lr)
+        # automatic entropy tuning
+        self.target_entropy = -np.prod((action_dim, )).item()  # heuristic
+        self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
+        self.alpha_optimizer = Adam([self.log_alpha], lr=3e-4)
 
         self.device = device
 
@@ -159,16 +157,14 @@ class Agent(object):
             action = self.env.action_space.sample()
         else:
             obs = torch.from_numpy(obs).float().unsqueeze(0).to(self.device)
-            probs = self.actor(obs)
-            action_dist = Categorical(probs)
-            action = action_dist.sample().item()
+            action, probs = self.actor(obs)
+            action = action.detach().cpu().numpy()
         return action
 
     def predict(self, obs) -> int:
         obs = torch.from_numpy(obs).float().unsqueeze(0).to(self.device)
-        probs = self.actor(obs)
-        action_dist = Categorical(probs)
-        action = action_dist.sample().item()
+        action, probs = self.actor(obs)
+        action = action.detach().cpu().numpy()
         return action
 
     # Set up function for computing SAC Q-losses
@@ -199,27 +195,14 @@ class Agent(object):
 
     # Set up function for computing SAC pi loss
     def compute_actor_loss(self, obs: torch.Tensor) -> torch.Tensor:
-        pred_action, log_prob = self.actor(obs)
-        q1_pi = self.critic1(obs, pred_action)
-        q2_pi = self.critic2(obs, pred_action)
+        next_action, log_prob = self.actor(obs)
+        q1_pi = self.critic1(obs, next_action)
+        q2_pi = self.critic2(obs, next_action)
         q_pi = torch.min(q1_pi, q2_pi)
 
         # Entropy-regularized policy loss
         actor_loss = (self.alpha * log_prob - q_pi).mean()
         return actor_loss
-
-    # 计算目标Q值,直接用策略网络的输出概率进行期望计算
-    def calc_target(self, rewards, next_obs, terminal):
-        next_probs = self.actor(next_obs)
-        prob_dist = Categorical(next_probs)
-        entropy = prob_dist.entropy()
-        q1_value = self.target_critic1(next_obs)
-        q2_value = self.target_critic2(next_obs)
-        min_qvalue = torch.sum(
-            next_probs * torch.min(q1_value, q2_value), dim=1, keepdim=True)
-        next_value = min_qvalue + self.log_alpha.exp() * entropy
-        td_target = rewards + self.gamma * next_value * (1 - terminal)
-        return td_target
 
     def soft_update(self, net, target_net):
         for param_target, param in zip(target_net.parameters(),
@@ -230,7 +213,6 @@ class Agent(object):
     def learn(self, obs: np.ndarray, action: np.ndarray, reward: np.ndarray,
               next_obs: np.ndarray,
               terminal: np.ndarray) -> Tuple[float, float]:
-        """Update the model by TD actor-critic."""
 
         device = self.device  # for shortening the following lines
         obs = torch.FloatTensor(obs).to(device)
@@ -239,72 +221,60 @@ class Agent(object):
         rewards = torch.FloatTensor(reward.reshape(-1, 1)).to(device)
         terminal = torch.FloatTensor(terminal.reshape(-1, 1)).to(device)
 
-        # 更新两个Q网络
-        td_target = self.calc_target(rewards, next_obs, terminal)
-        critic1_q_values = self.critic1(obs).gather(1, actions)
-        critic1_loss = F.mse_loss(critic1_q_values, td_target.detach())
-        critic2_q_values = self.critic2(obs).gather(1, actions)
-        critic2_loss = F.mse_loss(critic2_q_values, td_target.detach())
+        new_action, log_prob = self.actor(obs)
 
-        # critic1_loss backward
-        self.critic1_optimizer.zero_grad()
-        critic1_loss.backward()
+        # train alpha (dual problem)
+        alpha_loss = (-self.log_alpha.exp() *
+                      (log_prob + self.target_entropy).detach()).mean()
+
+        self.alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.alpha_optimizer.step()
+
+        alpha = self.log_alpha.exp()  # used for the actor loss calculation
+
+        pred_q1_value = self.critic1(obs, actions)
+        pred_q2_value = self.critic2(obs, actions)
+        # Bellman backup for Q functions
+        with torch.no_grad():
+            # Target actions come from *current* policy
+            next_action, next_action_logp = self.actor(next_obs)
+            # Target Q-values
+            q1_target = self.target_critic1(next_obs, next_action)
+            q2_target = self.target_critic2(next_obs, next_action)
+
+            q_target = torch.min(q1_target, q2_target)
+            td_target = rewards + self.gamma * (
+                q_target - self.alpha * next_action_logp) * (1 - terminal)
+
+        # MSE loss against Bellman backup
+        q1_loss = F.mse_loss(pred_q1_value, td_target.detach())
+        q2_loss = F.mse_loss(pred_q2_value, td_target.detach())
+        q_loss = q1_loss + q2_loss
+
+        # actor loss
+        q1_pi = self.critic1(obs, new_action)
+        q2_pi = self.critic2(obs, new_action)
+        q_pi = torch.min(q1_pi, q2_pi)
+        # Entropy-regularized policy loss
+        actor_loss = (alpha * log_prob - q_pi).mean()
+
+        #  train Q functions
+        # First run one gradient descent step for Q1 and Q2
+        self.critic1.zero_grad()
+        # q_loss = self.compute_critic_loss(obs, actions, rewards, next_obs,
+        #                                   terminal)
+        q_loss.backward()
         self.critic1_optimizer.step()
-        # critic1_loss backward
-        self.critic2_optimizer.zero_grad()
-        critic2_loss.backward()
-        self.critic2_optimizer.step()
 
-        # 更新策略网络
-        probs = self.actor(obs)
-        probs_dist = Categorical(probs)
-        # 直接根据概率计算熵
-        entropy = probs_dist.entropy()
-        q1_value = self.critic1(obs)
-        q2_value = self.critic2(obs)
-        min_qvalue = torch.sum(
-            probs * torch.min(q1_value, q2_value), dim=1, keepdim=True)
-        # 直接根据概率计算期望
-        actor_loss = torch.mean(-self.log_alpha.exp() * entropy - min_qvalue)
-        # backward
-        self.actor_optimizer.zero_grad()
+        # train actor
+        self.actor.zero_grad()
+        # actor_loss = self.compute_actor_loss(obs)
         actor_loss.backward()
         self.actor_optimizer.step()
-
-        # 更新alpha值
-        alpha_loss = torch.mean(
-            (entropy - self.target_entropy).detach() * self.log_alpha.exp())
-        self.log_alpha_optimizer.zero_grad()
-        alpha_loss.backward()
-        self.log_alpha_optimizer.step()
 
         self.soft_update(self.critic1, self.target_critic1)
         self.soft_update(self.critic2, self.target_critic2)
         self.global_update_step += 1
 
-        return actor_loss.item(), critic1_loss.item(), critic2_loss.item()
-
-    def update(self, obs: np.ndarray, action: np.ndarray, reward: np.ndarray,
-               next_obs: np.ndarray,
-               terminal: np.ndarray) -> Tuple[float, float]:
-
-        device = self.device  # for shortening the following lines
-        obs = torch.FloatTensor(obs).to(device)
-        next_obs = torch.FloatTensor(next_obs).to(device)
-        actions = torch.LongTensor(action.reshape(-1, 1)).to(device)
-        rewards = torch.FloatTensor(reward.reshape(-1, 1)).to(device)
-        terminal = torch.FloatTensor(terminal.reshape(-1, 1)).to(device)
-
-        # First run one gradient descent step for Q1 and Q2
-        self.critic1.zero_grad()
-        q_loss = self.compute_critic_loss(obs, actions, rewards, next_obs,
-                                          terminal)
-        q_loss.backward()
-        self.critic1_optimizer.step()
-
-        # Next run one gradient descent step for pi.
-        self.actor.zero_grad()
-        actor_loss = self.compute_actor_loss(obs)
-        actor_loss.backward()
-        self.actor_optimizer.step()
-        return
+        return actor_loss.item(), q_loss.item()
