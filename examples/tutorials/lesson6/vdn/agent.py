73a6b0a402234595c4665eaf5d5d6886a9bbaaf8
jianzhnie <jianzhnie@126.com>
2022-09-30 15:05:02 +0800

    add vdn algo

---
+++ examples/tutorials/lesson6/vdn/agent.py
@@ -0,0 +1,167 @@
+import copy
+
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+
+from rltoolkit.models.utils import hard_target_update
+from rltoolkit.utils.scheduler import LinearDecayScheduler
+
+
+class QNet(nn.Module):
+
+    def __init__(self,
+                 observation_space,
+                 action_space,
+                 hidden_size=32,
+                 recurrent=False):
+        super(QNet, self).__init__()
+        self.num_agents = len(observation_space)
+        self.hidden_size = hidden_size
+        self.recurrent = recurrent
+        for agent_i in range(self.num_agents):
+            obs_dim = observation_space[agent_i].shape[0]
+            setattr(
+                self, 'agent_feature_{}'.format(agent_i),
+                nn.Sequential(
+                    nn.Linear(obs_dim, 64), nn.ReLU(),
+                    nn.Linear(64, hidden_size)))
+
+            if recurrent:
+                setattr(self, 'agent_gru_{}'.format(agent_i),
+                        nn.GRUCell(hidden_size, hidden_size))
+            setattr(self, 'agent_q_{}'.format(agent_i),
+                    nn.Linear(hidden_size, action_space[agent_i].n))
+
+    def forward(self, obs, hidden):
+        q_values = [torch.empty(obs.shape[0], )] * self.num_agents
+        next_hidden = [torch.empty(obs.shape[0], 1, self.hidden_size)
+                       ] * self.num_agents
+        for agent_i in range(self.num_agents):
+            x = getattr(self, 'agent_feature_{}'.format(agent_i))(
+                obs[:, agent_i, :])
+            if self.recurrent:
+                x = getattr(self,
+                            'agent_gru_{}'.format(agent_i))(x,
+                                                            hidden[:,
+                                                                   agent_i, :])
+                next_hidden[agent_i] = x.unsqueeze(1)
+            q_values[agent_i] = getattr(
+                self, 'agent_q_{}'.format(agent_i))(x).unsqueeze(1)
+
+        # batch_size * num_agents * acion_dim
+        q_value = torch.cat(q_values, dim=1)
+        hidden = torch.cat(next_hidden, dim=1)
+
+        return q_value, hidden
+
+    def init_hidden(self, batch_size=1):
+        return torch.zeros((batch_size, self.num_agents, self.hidden_size))
+
+
+class Agent(object):
+
+    def __init__(self,
+                 env: gym.Env,
+                 gamma: float = 0.99,
+                 epsilon: float = 1.0,
+                 min_epsilon: float = 0.1,
+                 learning_rate: float = 0.001,
+                 batch_size: int = 32,
+                 chunk_size: int = 10,
+                 grad_clip_norm: float = 5.0,
+                 total_steps: int = int(1e4),
+                 update_target_step: int = 100,
+                 device='cpu'):
+        super().__init__()
+
+        self.env = env
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.curr_epsilon = epsilon
+        self.min_epsilon = min_epsilon
+        self.batch_size = batch_size
+        self.grad_clip_norm = grad_clip_norm
+        self.global_update_step = 0
+        self.update_target_step = update_target_step
+
+        # Main network
+        self.qnet = QNet(env.observation_space, env.action_space).to(device)
+        # Target network
+        self.target_qnet = copy.deepcopy(self.qnet)
+        # Create an optimizer
+        self.optimizer = Adam(self.qnet.parameters(), lr=learning_rate)
+        self.ep_scheduler = LinearDecayScheduler(epsilon, total_steps)
+
+        self._chunk_size = chunk_size if self.qnet.recurrent else 1
+
+        self.device = device
+
+    def sample(self, obs):
+
+        if np.random.rand() <= self.curr_epsilon:
+            action = self.env.action_space.sample()
+        else:
+            action = self.predict(obs)
+
+        # Decaying epsilon
+        self.curr_epsilon = max(self.ep_scheduler.step(1), self.min_epsilon)
+        return action
+
+    def predict(self, obs):
+        obs = torch.tensor(obs, dtype=torch.float).unsqueeze(0)
+        hidden = self.qnet.init_hidden()
+        q_value, hidden = self.qnet(obs, hidden)
+        action = q_value.argmax(dim=2)
+        action = action[0].cpu().numpy().tolist()
+        return action
+
+    def learn(self, obs: np.ndarray, action: np.ndarray, reward: np.ndarray,
+              next_obs: np.ndarray, terminal: np.ndarray):
+
+        if self.global_update_step % self.update_target_step == 0:
+            hard_target_update(self.qnet, self.target_qnet)
+
+        device = self.device  # for shortening the following lines
+        obs = torch.FloatTensor(obs).to(device)
+        next_obs = torch.FloatTensor(next_obs).to(device)
+        action = torch.LongTensor(action).to(device)
+        reward = torch.FloatTensor(reward).to(device)
+        terminal = torch.FloatTensor(terminal).to(device)
+
+        hidden = self.qnet.init_hidden(self.batch_size)
+        target_hidden = self.target_qnet.init_hidden(self.batch_size)
+
+        loss = 0
+        for step_i in range(self._chunk_size):
+            q_out, hidden = self.qnet(obs[:, step_i, :, :], hidden)
+
+            q_action = q_out.gather(
+                2, action[:, step_i, :].unsqueeze(-1)).squeeze(-1)
+            sum_q = q_action.sum(dim=1, keepdims=True)
+
+            next_q_value, target_hidden = self.target_qnet(
+                next_obs[:, step_i, :, :], target_hidden.detach())
+
+            max_q_value = next_q_value.max(dim=2)[0].squeeze(-1)
+            target_q = reward[:, step_i, :].sum(dim=1, keepdims=True)
+            target_q += self.gamma * max_q_value.sum(
+                dim=1, keepdims=True) * (1 - terminal[:, step_i])
+
+            loss += F.smooth_l1_loss(sum_q, target_q.detach())
+
+            done_mask = terminal[:, step_i].squeeze(-1).bool()
+            hidden[done_mask] = self.qnet.init_hidden(len(hidden[done_mask]))
+            target_hidden[done_mask] = self.target_qnet.init_hidden(
+                len(target_hidden[done_mask]))
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_norm_(
+            self.qnet.parameters(), self.grad_clip_norm, norm_type=2)
+        self.optimizer.step()
+
+        self.global_update_step += 1
---
+++ examples/tutorials/lesson6/vdn/train.py
@@ -0,0 +1,167 @@
+import argparse
+import sys
+
+import gym
+import numpy as np
+import torch
+import wandb
+from tqdm import tqdm
+
+sys.path.append('../../../../')
+from agent import Agent
+
+from rltoolkit.data.buffer.ma_replaybuffer import ReplayBuffer
+
+config = {
+    'train_seed': 42,
+    'test_seed': 42,
+    'env': 'ma_gym:Switch2-v1',
+    'use_wandb': False,
+    'algo': 'dqn',
+    'hidden_dim': 128,
+    'total_steps': 1000000,  # max training steps
+    'memory_size': 50000,  # Replay buffer size
+    'memory_warmup_size': 10000,  # Replay buffer memory_warmup_size
+    'batch_size': 32,  # repaly sample batch size
+    'log_interval': 10,
+    'update_target_step': 100,  # target model update freq
+    'learning_rate': 0.0005,  # start learning rate
+    'epsilon': 1,  # start greedy epsilon
+    'min_epsilon': 0.1,
+    'gamma': 0.99,  # discounting factor
+    'eval_render': True,  # do eval render
+    'video_folder': 'results'
+}
+
+
+# train an episode
+def run_train_episode(agent: Agent, env: gym.Env, rpm: ReplayBuffer,
+                      memory_warmup_size: int):
+
+    score = np.zeros(env.n_agents)
+    step = 0
+    obs = env.reset()
+    done = [False for _ in range(env.n_agents)]
+    while not all(done):
+        step += 1
+        action = agent.sample(obs)
+        next_obs, reward, done, _ = env.step(action)
+        rpm.store(obs, action, reward, next_obs, done)
+        # train model
+        if rpm.size() > memory_warmup_size:
+            # s,a,r,s',done
+            samples = rpm.sample_chunk(agent._chunk_size)
+            batch_obs = samples['obs']
+            batch_action = samples['action']
+            batch_reward = samples['reward']
+            batch_next_obs = samples['next_obs']
+            batch_terminal = samples['terminal']
+
+            print(batch_action.shape)
+            print(batch_obs.shape)
+            agent.learn(batch_obs, batch_action, batch_reward, batch_next_obs,
+                        batch_terminal)
+        score += np.array(reward)
+        obs = next_obs
+    return sum(score), step
+
+
+def run_evaluate_episodes(agent: Agent,
+                          env: gym.Env,
+                          n_eval_episodes: int = 5,
+                          render: bool = False,
+                          video_folder: str = None):
+    if video_folder is not None:
+        env = gym.wrappers.RecordVideo(env, video_folder=video_folder)
+
+    score = np.zeros(env.n_agents)
+    for _ in range(n_eval_episodes):
+        obs = env.reset()
+        done = [False for _ in range(env.n_agents)]
+        while not all(done):
+            action = agent.predict(obs)
+            next_obs, reward, done, _ = env.step(action)
+            obs = next_obs
+            score += np.array(reward)
+            if render:
+                env.render()
+    return sum(score / n_eval_episodes)
+
+
+def main():
+    args = argparse.Namespace(**config)
+    env = gym.make(args.env)
+    test_env = gym.make(args.env)
+    # set seed
+    torch.manual_seed(args.train_seed)
+    torch.cuda.manual_seed_all(args.train_seed)
+    env.seed(args.train_seed)
+    test_env.seed(args.test_seed)
+
+    device = torch.device(
+        'cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    if args.use_wandb:
+        wandb.init(project='minimal-marl', config=config, monitor_gym=True)
+
+    obs_dim = env.observation_space[0].shape[0]
+    num_agents = len(env.observation_space)
+
+    rpm = ReplayBuffer(
+        obs_dim=obs_dim,
+        num_agents=num_agents,
+        max_size=args.memory_size,
+        batch_size=args.batch_size)
+
+    # get agent
+    agent = Agent(
+        env=env,
+        gamma=args.gamma,
+        epsilon=args.epsilon,
+        min_epsilon=args.min_epsilon,
+        learning_rate=args.learning_rate,
+        total_steps=args.total_steps,
+        update_target_step=args.update_target_step,
+        device=device)
+
+    # start training, memory warm up
+    with tqdm(
+            total=args.memory_warmup_size,
+            desc='[Replay Memory Warm Up]') as pbar:
+        while rpm.size() < args.memory_warmup_size:
+            rewards, steps = run_train_episode(
+                agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
+            pbar.update(steps)
+
+    pbar = tqdm(total=args.total_steps)
+    episode_cnt = 0
+    cum_steps = 0  # this is the current timestep
+    episode_score = 0
+    while cum_steps < args.total_steps:
+        # start epoch
+        episode_cnt += 1
+        rewards, steps = run_train_episode(
+            agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
+        episode_score += rewards
+        cum_steps += steps
+        pbar.update(steps)
+        if episode_cnt % args.log_interval == 0:
+            train_score = episode_score / args.log_interval
+            test_score = run_evaluate_episodes(agent, test_env)
+            if args.use_wandb:
+                wandb.log({
+                    'episode': episode_cnt,
+                    'epsilon': agent.curr_epsilon,
+                    'test-score': test_score,
+                    'train-score': train_score
+                })
+            episode_score = 0
+
+    pbar.close()
+    # render and record video
+    run_evaluate_episodes(agent, test_env, n_eval_episodes=1, render=True)
+
+
+if __name__ == '__main__':
+
+    main()
