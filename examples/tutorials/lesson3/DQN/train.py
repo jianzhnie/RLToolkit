81384ecfff4a0f5225ebcfaba31339692ca24d6f
jianzhnie <jianzhnie@126.com>
2022-10-11 11:11:39 +0800

    Update train.py

--- examples/tutorials/lesson3/DQN/train.py
+++ examples/tutorials/lesson3/DQN/train.py
@@ -12,24 +12,32 @@ from agent import Agent
 from rltoolkit.data.buffer.replaybuffer import ReplayBuffer
 from rltoolkit.utils import logger, tensorboard
 
+try:
+    import wandb
+    has_wandb = True
+except ImportError:
+    has_wandb = False
+
 config = {
     'train_seed': 42,
     'test_seed': 42,
     'env': 'CartPole-v0',
     'algo': 'dqn',
+    'use_wandb': True,
     'hidden_dim': 128,
-    'total_steps': 10000,  # max training steps
+    'total_steps': 12000,  # max training steps
     'memory_size': 10000,  # Replay buffer size
     'memory_warmup_size': 1000,  # Replay buffer memory_warmup_size
     'batch_size': 64,  # repaly sample batch size
     'update_target_step': 100,  # target model update freq
     'learning_rate': 0.001,  # start learning rate
     'epsilon': 1,  # start greedy epsilon
-    'epsilon_decay': 0.9995,  # epsilon decay rate
+    'epsilon_decay': 0.95,  # epsilon decay rate
     'min_epsilon': 0.1,
     'gamma': 0.99,  # discounting factor
     'eval_render': True,  # do eval render
-    'test_every_steps': 1000,  # evaluation freq
+    'train_log_interval': 1,
+    'test_log_interval': 5,  # evaluation freq
     'video_folder': 'results'
 }
 
@@ -73,6 +81,7 @@ def run_evaluate_episodes(agent: Agent,
         env = gym.wrappers.RecordVideo(env, video_folder=video_folder)
     eval_rewards = []
     for _ in range(n_eval_episodes):
+        env.seed(np.random.randint(100))
         obs = env.reset()
         done = False
         score = 0
@@ -84,7 +93,7 @@ def run_evaluate_episodes(agent: Agent,
             if render:
                 env.render()
             if done:
-                obs = env.close()
+                env.close()
         eval_rewards.append(score)
     mean_reward = np.mean(eval_rewards)
     std_reward = np.std(eval_rewards)
@@ -104,6 +113,18 @@ def main():
     device = torch.device(
         'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+    if args.use_wandb:
+        if has_wandb:
+            wandb.init(
+                project=args.env,
+                config=args,
+                entity='jianzhnie',
+                monitor_gym=True)
+        else:
+            logger.warning(
+                "You've requested to log metrics to wandb but package not found. "
+                'Metrics not being logged to wandb, try `pip install wandb`')
+
     obs_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
     rpm = ReplayBuffer(
@@ -117,8 +138,6 @@ def main():
         algo=args.algo,
         gamma=args.gamma,
         epsilon=args.epsilon,
-        epsilon_decay=args.epsilon_decay,
-        min_epsilon=args.min_epsilon,
         learning_rate=args.learning_rate,
         update_target_step=args.update_target_step,
         device=device)
@@ -133,29 +152,38 @@ def main():
             pbar.update(steps)
 
     pbar = tqdm(total=args.total_steps)
+    episode_cnt = 0
     cum_steps = 0  # this is the current timestep
-    test_flag = 0
     while cum_steps < args.total_steps:
         # start epoch
+        episode_cnt += 1
         total_reward, steps = run_train_episode(
             agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
+
+        agent.epsilon *= args.epsilon_decay
+        agent.epsilon = max(agent.epsilon, args.min_epsilon)
+
         cum_steps += steps
-
-        logger.info(
-            '[Train], steps: {}, exploration:{}, learning rate:{}, Reward Sum {}.'
-            .format(cum_steps, agent.epsilon,
-                    agent.optimizer.param_groups[0]['lr'], total_reward))
-        tensorboard.add_scalar('{}/training_rewards'.format(args.algo),
-                               total_reward, cum_steps)
-        tensorboard.add_scalar('{}/exploration'.format(args.algo),
-                               agent.epsilon, cum_steps)
-
         pbar.update(steps)
+        if episode_cnt % args.train_log_interval == 0:
+            logger.info(
+                '[Train], steps: {}, exploration:{}, learning rate:{}, Reward Sum {}.'
+                .format(cum_steps, agent.epsilon,
+                        agent.optimizer.param_groups[0]['lr'], total_reward))
+            tensorboard.add_scalar('{}/training_rewards'.format(args.algo),
+                                   total_reward, cum_steps)
+            tensorboard.add_scalar('{}/exploration'.format(args.algo),
+                                   agent.epsilon, cum_steps)
+
+            if args.use_wandb:
+                wandb.log({
+                    'epsilon': agent.epsilon,
+                    'steps': cum_steps,
+                    'train-score': total_reward
+                })
 
         # perform evaluation
-        if cum_steps // args.test_every_steps >= test_flag:
-            while cum_steps // args.test_every_steps >= test_flag:
-                test_flag += 1
+        if episode_cnt % args.test_log_interval == 0:
             mean_reward, std_reward = run_evaluate_episodes(agent, test_env)
             logger.info('[Eval], steps: {}, mean: {}, std: {:.2f}'.format(
                 cum_steps, mean_reward, std_reward))
@@ -163,6 +191,12 @@ def main():
                 '{}/mean_validation_rewards'.format(args.algo), mean_reward,
                 cum_steps)
 
+            if args.use_wandb:
+                wandb.log({'test-score': mean_reward})
+                wandb.log({'test-score-upper': mean_reward + std_reward})
+                wandb.log({'test-score-lower': mean_reward - std_reward})
+
+    pbar.close()
     # render and record video
     run_evaluate_episodes(
         agent,
@@ -170,7 +204,6 @@ def main():
         n_eval_episodes=1,
         render=True,
         video_folder=args.video_folder)
-    pbar.close()
 
 
 if __name__ == '__main__':
