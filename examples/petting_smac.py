b3825435ad880d35b3b4eb5ddcd084931aa90a4f
jianzhnie <jianzhnie@126.com>
2022-12-12 18:43:49 +0800

    update smac dmo

--- examples/petting_smac.py
+++ examples/petting_smac.py
@@ -1,16 +1,13 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
 import random
+
 import numpy as np
 from smac.env.pettingzoo import StarCraft2PZEnv
 
 
 def main():
-    """
-    Runs an env object with random actions.
-    """
+    """Runs an env object with random actions."""
     env = StarCraft2PZEnv.env()
     episodes = 10
 
@@ -27,8 +24,8 @@ def main():
             total_reward += reward
             if terms or truncs:
                 action = None
-            elif isinstance(obs, dict) and "action_mask" in obs:
-                action = random.choice(np.flatnonzero(obs["action_mask"]))
+            elif isinstance(obs, dict) and 'action_mask' in obs:
+                action = random.choice(np.flatnonzero(obs['action_mask']))
             else:
                 action = env.action_spaces[agent].sample()
             env.step(action)
@@ -37,8 +34,8 @@ def main():
 
     env.close()
 
-    print("Average total reward", total_reward / episodes)
+    print('Average total reward', total_reward / episodes)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- examples/smac_demo.py
+++ examples/smac_demo.py
@@ -1,18 +1,17 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
-from smac.env import StarCraft2Env
 import numpy as np
+from smac.env import StarCraft2Env
+
 
 # 独立的智能体在接收到观察和全局状态后会执行随机策略。
 def main():
-    env = StarCraft2Env(map_name="8m")
+    env = StarCraft2Env(map_name='8m')
     env_info = env.get_env_info()
 
-    n_actions = env_info["n_actions"]  # 获取动作维度 14
-    n_agents = env_info["n_agents"]  # 存在多少个智能体 8
-    print("n_agents: %d, n_actions: %d" % (n_agents, n_actions))
+    n_actions = env_info['n_actions']  # 获取动作维度 14
+    n_agents = env_info['n_agents']  # 存在多少个智能体 8
+    print('n_agents: %d, n_actions: %d' % (n_agents, n_actions))
     n_episodes = 10
     for e in range(n_episodes):
         env.reset()
@@ -26,7 +25,7 @@ def main():
             state = env.get_state()
             env.render()  # Uncomment for rendering
             actions = []
-            for agent_id in range(n_agents): # 对于每个智能体遍历循环
+            for agent_id in range(n_agents):  # 对于每个智能体遍历循环
                 avail_actions = env.get_avail_agent_actions(agent_id)
                 avail_actions_ind = np.nonzero(avail_actions)[0]
                 action = np.random.choice(avail_actions_ind)
@@ -35,10 +34,10 @@ def main():
             reward, terminated, _ = env.step(actions)
             episode_reward += reward
 
-        print("Total reward in episode {} = {}".format(e, episode_reward))
+        print('Total reward in episode {} = {}'.format(e, episode_reward))
 
     env.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
