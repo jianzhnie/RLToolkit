d8ed9329ca7e0b8bcd9fbc0cc26aae2abe27aa5f
jianzhnie <jianzhnie@126.com>
2022-12-12 18:05:55 +0800

    update mral agents

--- benchmark/coma/starcraft2/build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#### -------------------------------------------------------------------
-#### build docker image
-#### -------------------------------------------------------------------
-echo 'Building Dockerfile with image name parl-starcraft2:1.0'
-docker build -t parl-starcraft2:1.0 .
