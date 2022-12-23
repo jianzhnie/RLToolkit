52fd44819524de08564c0306f2973354922604ee
jianzhnie <jianzhnie@126.com>
2022-12-23 18:10:24 +0800

    update coma

--- examples/marl/coma/sc2_agent.py
+++ examples/marl/coma/sc2_agent.py
@@ -10,7 +10,7 @@ from copy import deepcopy
 
 import numpy as np
 import torch
-from torch.distributions import Categorical
+from torch import nn
 from torch.optim import RMSprop
 
 from rltoolkit.models.utils import check_model_method
@@ -19,15 +19,19 @@ from rltoolkit.models.utils import check_model_method
 class Agents(object):
 
     def __init__(self,
-                 model,
-                 n_actions,
-                 n_agents,
-                 actor_lr=None,
-                 critic_lr=None,
-                 gamma=None,
-                 td_lambda=None,
-                 grad_norm_clip=None,
-                 device='cpu'):
+                 agent_model: nn.Module = None,
+                 critic_model: nn.Module = None,
+                 n_agents: int = None,
+                 n_actions: int = None,
+                 actor_lr: float = None,
+                 critic_lr: float = None,
+                 gamma: float = None,
+                 td_lambda: float = None,
+                 agent_output_type: str = 'pi_logits',
+                 grad_norm_clip: float = None,
+                 optim_alpha: float = 0.99,
+                 optim_eps: float = 0.00001,
+                 device: str = 'cpu'):
         """COMA Agent.
 
         Args:
@@ -41,12 +45,10 @@ class Agents(object):
             td_lambda (float): lambda of td-lambda return
         """
         # checks
-        check_model_method(model, 'value', self.__class__.__name__)
-        check_model_method(model, 'policy', self.__class__.__name__)
-        check_model_method(model, 'get_actor_params', self.__class__.__name__)
-        check_model_method(model, 'get_critic_params', self.__class__.__name__)
-        assert isinstance(n_actions, int)
+        check_model_method(agent_model, 'forward', self.__class__.__name__)
+        check_model_method(critic_model, 'forward', self.__class__.__name__)
         assert isinstance(n_agents, int)
+        assert isinstance(n_actions, int)
         assert isinstance(actor_lr, float)
         assert isinstance(critic_lr, float)
         assert isinstance(gamma, float)
@@ -60,20 +62,28 @@ class Agents(object):
         self.critic_lr = critic_lr
         self.gamma = gamma
         self.td_lambda = td_lambda
+        self.agent_output_type = agent_output_type
         self.grad_norm_clip = grad_norm_clip
-        self.train_steps = 0
-        self.rnn_h = None
-        self.device = device
 
-        self.model = model.to(self.device)
-        self.target_model = deepcopy(model).to(self.device)
+        self.agent_model = agent_model.to(device)
+        self.target_agent_model = deepcopy(agent_model).to(device)
+        self.critic_model = critic_model.to(device)
+        self.target_critic_model = deepcopy(critic_model).to(device)
 
-        self.actor_parameters = list(self.model.get_actor_params())
-        self.critic_parameters = list(self.model.get_critic_params())
+        self.actor_parameters = list(self.agent_model.parameters())
+        self.critic_parameters = list(self.critic_model.parameters())
+
+        self.actor_optimizer = RMSprop(
+            self.actor_parameters,
+            lr=self.actor_lr,
+            alpha=optim_alpha,
+            eps=optim_eps)
 
         self.critic_optimizer = RMSprop(
-            self.critic_parameters, lr=self.critic_lr)
-        self.actor_optimizer = RMSprop(self.actor_parameters, lr=self.actor_lr)
+            self.critic_parameters,
+            lr=self.critic_lr,
+            alpha=optim_alpha,
+            eps=optim_eps)
 
     def init_hidden(self, ep_num=1):
         """ function: init a hidden tensor for every agent
@@ -106,183 +116,112 @@ class Agents(object):
             # shape [1, 9]
         return prob, rnn_h_out
 
-    def sample(self,
-               obs,
-               last_action,
-               agent_id,
-               avail_actions,
-               epsilon,
-               test=False):
-        """input:
-                obs (array): agent i's obs
-                last_action (int): agent i's last action
-                agent_id (int): agent index
-                avail_actions (one_hot): available actions
-                epsilon (float): e_greed discount
-                test (bool): train or test
-            output:
-                action: int
-                prob: probability of every action, float, 0 ~ 1
-        """
-        obs = obs.copy()
-        # make obs: obs + agent's last action(one_hot) + agent's id(one_hot)
-        last_act_one_hot = np.zeros(self.n_actions)
-        last_act_one_hot[last_action] = 1.
-        id_one_hot = np.zeros(self.n_agents)
-        id_one_hot[agent_id] = 1.
-        obs = np.hstack((obs, last_act_one_hot))
-        obs = np.hstack((obs, id_one_hot))
+    def learn(self, state_batch, actions_batch, reward_batch, terminated_batch,
+              obs_batch, available_actions_batch, filled_batch):
+        '''
+        Args:
+            state (np.ndarray):                   (batch_size, T, state_shape)
+            actions (np.ndarray):                 (batch_size, T, n_agents)
+            reward (np.ndarray):                  (batch_size, T, 1)
+            terminated (np.ndarray):              (batch_size, T, 1)
+            obs (np.ndarray):                     (batch_size, T, n_agents, obs_shape)
+            available_actions_batch (np.ndarray): (batch_size, T, n_agents, n_actions)
+            filled_batch (np.ndarray):            (batch_size, T, 1)
+        Returns:
+            mean_loss (float): train loss
+            mean_td_error (float): train TD error
+        '''
 
-        # predict action prob
-        prob, self.rnn_h[agent_id] = self.predict(obs, self.rnn_h[agent_id])
+        # set the actions to torch.Long
+        actions_batch = actions_batch.to(self.device, dtype=torch.long)
+        # get the batch_size and episode_length
+        batch_size = state_batch.shape[0]
+        episode_len = state_batch.shape[1]
 
-        # add noise
-        avail_actions = torch.tensor(
-            avail_actions, dtype=torch.float32).unsqueeze(0).to(self.device)
-        # shape: (1, n_actions)
-        action_num = avail_actions.sum()  # how many actions are available
-        prob = ((1 - epsilon) * prob +
-                torch.ones_like(prob) * epsilon / action_num)
-        prob[avail_actions == 0] = 0.0  # set avail action
+        reward_batch = reward_batch[:, :-1, :]
+        actions_batch = actions_batch[:, :-1, :].unsqueeze(-1)
+        terminated_batch = terminated_batch[:, :-1, :]
+        filled_batch = filled_batch[:, :-1, :]
 
-        # choose action
-        if epsilon == 0 or test:
-            action = torch.argmax(prob)
+        mask = (1 - filled_batch) * (1 - terminated_batch)
+
+        # Calculate estimated Q-Values
+        local_qs = []
+        target_local_qs = []
+        self._init_hidden_states(batch_size)
+        for t in range(episode_len):
+            obs = obs_batch[:, t, :, :]
+            # obs: (batch_size * n_agents, obs_shape)
+            obs = obs.reshape(-1, obs_batch.shape[-1])
+            # Calculate estimated Q-Values
+            local_q, self.hidden_states = self.agent_model(
+                obs, self.hidden_states)
+            #  local_q: (batch_size * n_agents, n_actions) -->  (batch_size, n_agents, n_actions)
+            local_q = local_q.reshape(batch_size, self.n_agents, -1)
+            local_qs.append(local_q)
+
+            # Calculate the Q-Values necessary for the target
+            target_local_q, self.target_hidden_states = self.target_agent_model(
+                obs, self.target_hidden_states)
+            # target_local_q: (batch_size * n_agents, n_actions) -->  (batch_size, n_agents, n_actions)
+            target_local_q = target_local_q.view(batch_size, self.n_agents, -1)
+            target_local_qs.append(target_local_q)
+
+        # Concat over time
+        local_qs = torch.stack(local_qs, dim=1)
+        # We don't need the first timesteps Q-Value estimate for calculating targets
+        target_local_qs = torch.stack(target_local_qs[1:], dim=1)
+
+        # Softmax the agent outputs if they're policy logits
+        if self.agent_output_type == 'pi_logits':
+            local_qs_prob = torch.softmax(local_qs, dim=-1)
+
+        local_qs_prob[available_actions_batch == 0] = 0.0
+
+        # Pick the Q-Values for the actions taken by each agent
+        chosen_action_local_qs = torch.gather(
+            local_qs[:, :-1, :, :], dim=3, index=actions_batch).squeeze(3)
+
+        # mask unavailable actions
+        target_local_qs[available_actions_batch[:, 1:, :] == 0] = -1e10
+
+        # Max over target Q-Values
+        if self.double_q:
+            # Get actions that maximise live Q (for double q-learning)
+            local_qs_detach = local_qs.clone().detach()
+            local_qs_detach[available_actions_batch == 0] = -1e10
+            cur_max_actions = local_qs_detach[:, 1:].max(
+                dim=3, keepdim=True)[1]
+            target_local_max_qs = torch.gather(
+                target_local_qs, dim=3, index=cur_max_actions).squeeze(3)
         else:
-            action = Categorical(prob).sample().long()
-        return action.cpu()
+            # idx0: value, idx1: index
+            target_local_max_qs = target_local_qs.max(dim=3)[0]
 
-    def _get_actor_inputs(self, batch):
-        """o(t), u(t-1)_a, agent_id."""
-        obs = batch['o']
-        u_onehot = batch['u_onehot']
-        u_onehot_last = np.zeros_like(u_onehot)
-        u_onehot_last[:, 1:] = u_onehot[:, :-1]
-        ep_num = batch['o'].shape[0]
-        tr_num = batch['o'].shape[1]
+        # Mixing network
+        # mix_net, input: ([Q1, Q2, ...], state), output: Q_total
+        if self.mixer_model is not None:
+            chosen_action_global_qs = self.mixer_model(chosen_action_local_qs,
+                                                       state_batch[:, :-1, :])
+            target_global_max_qs = self.target_mixer_model(
+                target_local_max_qs, state_batch[:, 1:, :])
 
-        actor_inputs = []
-        for agent_id in range(self.n_agents):
-            obs_a = obs[:, :, agent_id]
-            u_a_onehot_last = u_onehot_last[:, :, agent_id]
-            id_onehot = np.zeros((ep_num, tr_num, self.n_agents))
-            id_onehot[:, :, agent_id] = 1.
-            # actor inputs: obs + agent's last action(one_hot) + agent's id(one_hot)
-            a_inputs = np.concatenate((obs_a, u_a_onehot_last, id_onehot),
-                                      axis=2)
-            # a_inpits shape (ep_num, tr_num, actor_input_dim)
-            actor_inputs.append(a_inputs)
+        # Calculate 1-step Q-Learning targets
+        target = reward_batch + self.gamma * (
+            1 - terminated_batch) * target_global_max_qs
+        #  Td-error
+        td_error = target.detach() - chosen_action_global_qs
+        #  0-out the targets that came from padded data
+        masked_td_error = td_error * mask
+        mean_td_error = masked_td_error.sum() / mask.sum()
+        # Normal L2 loss, take mean over actual data
+        loss = (masked_td_error**2).sum() / mask.sum()
 
-        actor_inputs = np.stack(actor_inputs, axis=2)
-        # shape (ep_num, tr_num, n_agents, actor_input_dim)
-        return actor_inputs
+        # Optimise
+        self.optimizer.zero_grad()
+        loss.backward()
+        if self.clip_grad_norm:
+            torch.nn.utils.clip_grad_norm_(self.params, self.clip_grad_norm)
+        self.optimizer.step()
 
-    def _get_critic_inputs(self, batch):
-        """o(t)_a, s(t), u(t)_-a, u(t-1), agent_id."""
-        ep_num = batch['o'].shape[0]
-        tr_num = batch['o'].shape[1]
-
-        # o, o_next, state, state_next
-        o = batch['o']  # shape (ep_num, tr_num, n_agents, obs_shape)
-        o_next = np.zeros_like(o)
-        o_next[:, :-1] = o[:, 1:]
-        s = batch['s']  # shape (ep_num, tr_num, state_shape)
-        s_next = np.zeros_like(s)
-        s_next[:, :-1] = s[:, 1:]
-        # u_onehot, u_onehot_last shape (ep_num, tr_num, n_agents, n_actions)
-        u_onehot = batch['u_onehot']
-        u_onehot_next = np.zeros_like(u_onehot)
-        u_onehot_next[:, :-1] = u_onehot[:, 1:]
-        u_onehot_last = np.zeros_like(u_onehot)
-        u_onehot_last[:, 1:] = u_onehot[:, :-1]
-
-        critic_inputs = []
-        critic_inputs_next = []
-        for agent_id in range(self.n_agents):
-            # get o(t)_a, s(t)
-            o_a = o[:, :, agent_id]  # shape (ep_num, tr_num, obs_shape)
-            o_a_next = o_next[:, :, agent_id]
-            s_a = s  # shape (ep_num, tr_num, state_shape)
-            s_a_next = s_next
-            # get u(t-1)  shape (ep_num, tr_num, n_agents * n_actions)
-            u_all_onehot = u_onehot.reshape(
-                (ep_num, tr_num, self.n_agents * self.n_actions))
-            u_all_onehot_next = u_onehot_next.reshape(
-                (ep_num, tr_num, self.n_agents * self.n_actions))
-            u_all_onehot_last = u_onehot_last.reshape(
-                (ep_num, tr_num, self.n_agents * self.n_actions))
-            # get u(t)_-a,   set 0 to mask action, shape (ep_num, tr_num, n_agents * n_actions)
-            u_not_a_onehot = u_all_onehot.copy()
-            u_not_a_onehot_next = u_all_onehot_next.copy()
-            m_s = agent_id * self.n_actions  # mask start flag
-            m_e = (agent_id + 1) * self.n_actions  # mask end flag
-            u_not_a_onehot[:, :, m_s:m_e] = 0
-            u_not_a_onehot_next[:, :, m_s:m_e] = 0
-            # get id onehot, shape (ep_num, tr_num, n_agents)
-            id_onehot = np.zeros((ep_num, tr_num, self.n_agents))
-            id_onehot[:, :, agent_id] = 1.
-
-            # input:      o,      s,      u_-a,      u_last, agent_id
-            # input_next: o_next, s_next, u_-a_next, u,      agent_id
-            # shape (ep_num, tr_num, critic_input_dim)
-            c_inputs = np.concatenate(
-                (o_a, s_a, u_not_a_onehot, u_all_onehot_last, id_onehot),
-                axis=2)
-            c_inputs_next = np.concatenate(
-                (o_a_next, s_a_next, u_not_a_onehot_next, u_all_onehot,
-                 id_onehot),
-                axis=2)
-            critic_inputs.append(c_inputs)
-            critic_inputs_next.append(c_inputs_next)
-        critic_inputs = np.stack(critic_inputs, axis=2)
-        critic_inputs_next = np.stack(critic_inputs_next, axis=2)
-        # shape (ep_num, tr_num, n_agents, critic_input_dim)
-        return critic_inputs, critic_inputs_next
-
-    def _get_avail_transitions_num(self, isover_batch):
-        """ input:
-                isover_batch: shape (ep_num, tr_num, 1)
-            output:
-                max_tr_num: max avail transitions number in all episodes
-        """
-        ep_num = isover_batch.shape[0]
-        max_tr_num = 0
-        for ep_id in range(ep_num):
-            for tr_id in range(self.config['episode_limit']):
-                if isover_batch[ep_id, tr_id, 0] == 1:
-                    if tr_id + 1 >= max_tr_num:
-                        max_tr_num = tr_id + 1
-                    break
-        return max_tr_num
-
-    def learn(self, batch, epsilon=None):
-        """ input:
-                batch: dict(o, s, u, r, u_onehot, avail_u, padded, isover)
-                epsilon: e-greedy discount
-        """
-        # different episode has different avail transition length
-        tr_num = self._get_avail_transitions_num(batch['isover'])
-        for key in batch.keys():
-            # cut batch data's episode length
-            batch[key] = batch[key][:, :tr_num]
-
-        # get actor input and critic input
-        batch['actor_inputs'] = self._get_actor_inputs(batch)
-        batch['critic_inputs'], batch[
-            'critic_inputs_next'] = self._get_critic_inputs(batch)
-
-        # change batch data to torch tensor
-        for key in batch.keys():
-            if key == 'u':
-                batch[key] = torch.tensor(
-                    batch[key], dtype=torch.long).to(self.device)
-            else:
-                batch[key] = torch.tensor(
-                    batch[key], dtype=torch.float32).to(self.device)
-
-        self.alg.learn(batch, epsilon)
-
-        if self.train_steps > 0 and self.train_steps % self.config[
-                'target_update_cycle'] == 0:
-            self.alg.sync_target()
-        self.train_steps += 1
+        return loss.item(), mean_td_error.item()
