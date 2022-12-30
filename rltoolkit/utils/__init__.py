674e98fbdd7b2764612f9da1ea78f64587b9cba9
jianzhnie <jianzhnie@126.com>
2022-12-30 16:14:48 +0800

    Update __init__.py

--- rltoolkit/utils/__init__.py
+++ rltoolkit/utils/__init__.py
@@ -1,11 +1,14 @@
 """Utils package."""
 
 from .logger.base import BaseLogger, LazyLogger
+from .logger.logs import get_logger, get_outdir
 from .logger.tensorboard import TensorboardLogger
 from .logger.wandb import WandbLogger
 from .scheduler import LinearDecayScheduler, PiecewiseScheduler
 
 __all__ = [
+    'get_logger',
+    'get_outdir',
     'BaseLogger',
     'TensorboardLogger',
     'LazyLogger',
