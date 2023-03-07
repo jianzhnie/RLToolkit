affc94e319923337a0f58151a61e72f0bf0e226c
jianzhnie <jianzhnie@126.com>
2023-03-07 19:18:47 +0800

    Update train.py

--- examples/tutorials/DDPG-BipedalWalker/train.py
+++ examples/tutorials/DDPG-BipedalWalker/train.py
@@ -1,46 +1,42 @@
 import argparse
-import sys
+import os
+import time
 
 import gym
+import mmcv
 import numpy as np
 import torch
 from ddpg_agent import Agent
-from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
 
-from rltoolkit.data.buffer.replaybuffer import ReplayBuffer
-
-sys.path.append('../../../../')
-from rltoolkit.utils import logger, tensorboard
-
-try:
-    import wandb
-    has_wandb = True
-except ImportError:
-    has_wandb = False
+from rltoolkit.data.buffer.replaybuffer import \
+    SimpleReplayBuffer as ReplayBuffer
+from rltoolkit.utils import TensorboardLogger, WandbLogger
+from rltoolkit.utils.logger.logs import get_outdir, get_root_logger
 
 config = {
     'train_seed': 42,
     'test_seed': 42,
+    'project': 'Classic-Control',
     'env': 'BipedalWalker-v3',
-    'algo': 'ddpgn',
-    'use_wandb': True,
+    'algo': 'ddpg',
     'hidden_dim': 128,
     'total_steps': 1000000,  # max training steps
     'memory_size': 200000,  # Replay buffer size
     'memory_warmup_size': 10000,  # Replay buffer memory_warmup_size
-    'actor_lr': 3e-4,  # start learning rate
-    'critic_lr': 3e-3,  # end learning rate
-    'initial_random_steps': 2000,
-    'ou_noise_theta': 1.0,
-    'ou_noise_sigma': 0.1,
+    'actor_lr': 5e-4,  # start learning rate
+    'critic_lr': 5e-3,  # end learning rate
+    'ou_noise_theta': 0.15,
+    'ou_noise_sigma': 0.3,
     'weight_decay': 0.0001,
     'gamma': 0.99,  # discounting factor
     'tau': 0.005,  # 软更新参数,
-    'sigma': 0.01,
     'batch_size': 128,
-    'eval_render': False,  # do eval render
     'train_log_interval': 1,
     'test_log_interval': 5,  # evaluation freq
+    'log_dir': 'work_dirs',
+    'logger': 'wandb',
+    'eval_render': False,  # do eval render
     'video_folder': 'results'
 }
 
@@ -48,14 +44,14 @@ config = {
 # train an episode
 def run_train_episode(agent: Agent, env: gym.Env, rpm: ReplayBuffer,
                       memory_warmup_size: int):
-    total_reward = 0
-    policy_loss_lst = []
-    value_loss_lst = []
-    step = 0
+    episode_reward = 0
+    episode_step = 0
+    episode_policy_loss = []
+    episode_value_loss = []
     obs = env.reset()
     done = False
     while not done:
-        step += 1
+        episode_step += 1
         action = agent.sample(obs)
         next_obs, reward, done, _ = env.step(action)
         rpm.append(obs, action, reward, next_obs, done)
@@ -73,41 +69,47 @@ def run_train_episode(agent: Agent, env: gym.Env, rpm: ReplayBuffer,
             policy_loss, value_loss = agent.learn(batch_obs, batch_action,
                                                   batch_reward, batch_next_obs,
                                                   batch_terminal)
-            policy_loss_lst.append(policy_loss)
-            value_loss_lst.append(value_loss)
+            episode_policy_loss.append(policy_loss)
+            episode_value_loss.append(value_loss)
 
-        total_reward += reward
+        episode_reward += reward
         obs = next_obs
-    return total_reward, step, np.mean(policy_loss_lst), np.mean(
-        value_loss_lst)
+    return episode_reward, episode_step, np.mean(episode_policy_loss), np.mean(
+        episode_value_loss)
 
 
 # 评估 agent, 跑 5 个episode，总reward求平均
-def evaluate(agent: Agent,
-             env: gym.Env,
-             n_eval_episodes: int = 5,
-             render: bool = False,
-             video_folder: str = None):
+def run_evaluate_episodes(agent: Agent,
+                          env: gym.Env,
+                          n_eval_episodes: int = 5,
+                          render: bool = False,
+                          video_folder: str = None):
 
     if video_folder is not None:
         env = gym.wrappers.RecordVideo(env, video_folder=video_folder)
     eval_rewards = []
+    eval_steps = []
     for _ in range(n_eval_episodes):
+        env.seed(np.random.randint(100))
         obs = env.reset()
         done = False
         episode_reward = 0
+        episode_step = 0
         while not done:
             action = agent.predict(obs)
-            obs, reward, done, _ = env.step(action)
+            next_obs, reward, done, _ = env.step(action)
+            obs = next_obs
             episode_reward += reward
+            episode_step += 1
             if render:
                 env.render()
             if done:
                 env.close()
         eval_rewards.append(episode_reward)
+        eval_steps.append(episode_step)
     mean_reward = np.mean(eval_rewards)
-    std_reward = np.std(eval_rewards)
-    return mean_reward, std_reward
+    mean_steps = np.mean(eval_steps)
+    return mean_reward, mean_steps
 
 
 def main():
@@ -124,22 +126,45 @@ def main():
     device = torch.device(
         'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    if args.use_wandb:
-        if has_wandb:
-            wandb.init(
-                project=args.env + '_' + args.algo,
-                config=args,
-                entity='jianzhnie',
-                sync_tensorboard=True,
-                monitor_gym=True)
-        else:
-            logger.warning(
-                "You've requested to log metrics to wandb but package not found. "
-                'Metrics not being logged to wandb, try `pip install wandb`')
+    # init the logger before other steps
+    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    # log
+    log_name = os.path.join(args.project, args.env, args.algo, timestamp)
+    text_log_path = os.path.join(args.log_dir, args.project, args.env,
+                                 args.algo)
+    tensorboard_log_path = get_outdir(text_log_path, 'tensorboard_log')
+    log_file = os.path.join(text_log_path, f'{timestamp}.log')
+    text_logger = get_root_logger(log_file=log_file, log_level='INFO')
+    args.video_folder = get_outdir(text_log_path, 'video')
+
+    if args.logger == 'wandb':
+        logger = WandbLogger(
+            dir=text_log_path,
+            train_interval=args.train_log_interval,
+            test_interval=args.test_log_interval,
+            update_interval=args.train_log_interval,
+            project=args.project,
+            name=log_name.replace(os.path.sep, '_'),
+            config=args,
+            entity='jianzhnie')
+    writer = SummaryWriter(tensorboard_log_path)
+    writer.add_text('args', str(args))
+    if args.logger == 'tensorboard':
+        logger = TensorboardLogger(writer)
+    else:  # wandb
+        logger.load(writer)
 
     obs_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
     action_bound = env.action_space.high[0]  # 动作最大值
+
+    print('---------------------------------------')
+    print('Environment:', args.env)
+    print('Algorithm:', args.algo)
+    print('State dimension:', obs_dim)
+    print('Action dimension:', action_dim)
+    print('Action limit:', action_bound)
+    print('---------------------------------------')
 
     rpm = ReplayBuffer(
         max_size=args.memory_size,
@@ -155,67 +180,60 @@ def main():
         hidden_dim=args.hidden_dim,
         actor_lr=args.actor_lr,
         critic_lr=args.critic_lr,
-        weight_decay=args.weight_decay,
-        initial_random_steps=args.initial_random_steps,
+        ou_noise_theta=args.ou_noise_theta,
+        ou_noise_sigma=args.ou_noise_sigma,
         action_bound=action_bound,
         tau=args.tau,
         gamma=args.gamma,
         device=device)
 
     # start training, memory warm up
-    with tqdm(
-            total=args.memory_warmup_size,
-            desc='[Replay Memory Warm Up]') as pbar:
-        while rpm.size() < args.memory_warmup_size:
-            total_reward, steps, _, _ = run_train_episode(
-                agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
-            pbar.update(steps)
-
-    pbar = tqdm(total=args.total_steps, desc='[Training]')
-    cum_steps = 0  # this is the current timestep
-    episode_cnt = 0
-    while cum_steps < args.total_steps:
-        # start epoch
-        episode_cnt += 1
-        total_reward, steps, policy_loss, value_loss = run_train_episode(
+    progress_bar = mmcv.ProgressBar(config['memory_warmup_size'])
+    while rpm.size() < args.memory_warmup_size:
+        episode_reward, episode_step, episode_policy_loss, episode_value_loss = run_train_episode(
             agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
-        cum_steps += steps
+        progress_bar.update(episode_step)
 
-        if episode_cnt % args.train_log_interval == 0:
-            logger.info(
-                'Current Steps: {}, Plicy Loss {:.2f}, Value Loss {:.2f}, Reward Sum {}.'
-                .format(cum_steps, policy_loss, value_loss, total_reward))
-            tensorboard.add_scalar('{}/training_rewards'.format(args.algo),
-                                   total_reward, cum_steps)
-            tensorboard.add_scalar('{}/policy_loss'.format(args.algo),
-                                   policy_loss, cum_steps)
-            tensorboard.add_scalar('{}/value_loss'.format(args.algo),
-                                   value_loss, cum_steps)
+    episode_cnt = 0
+    steps_cnt = 0  # this is the current timestep
+    progress_bar = mmcv.ProgressBar(args.total_steps)
+    while steps_cnt < args.total_steps:
+        # start epoch
+        episode_reward, episode_step, episode_policy_loss, episode_value_loss = run_train_episode(
+            agent, env, rpm, memory_warmup_size=args.memory_warmup_size)
 
-            if args.use_wandb:
-                wandb.log({'episode_length': steps})
-                wandb.log({'policy_loss': policy_loss})
-                wandb.log({'value_loss': value_loss})
-                wandb.log({'train_reward': total_reward})
+        steps_cnt += episode_step
+        episode_cnt += 1
 
-        pbar.update(steps)
+        train_results = {
+            'env_step': episode_step,
+            'rewards': episode_reward,
+            'episode_policy_loss': episode_policy_loss,
+            'episode_value_loss': episode_value_loss,
+            'replay_buffer_size': rpm.size()
+        }
+
+        if episode_cnt % config['train_log_interval'] == 0:
+            text_logger.info(
+                '[Train], episode: {},  train_reward: {:.2f}'.format(
+                    episode_cnt, episode_reward))
+            logger.log_train_data(train_results, steps_cnt)
+
         # perform evaluation
-        if episode_cnt % args.test_log_interval == 0:
-            mean_reward, std_reward = evaluate(agent, test_env)
-            logger.info(
-                'Eval_agent done, steps: {}, mean: {}, std: {:.2f}'.format(
-                    cum_steps, mean_reward, std_reward))
-            tensorboard.add_scalar(
-                '{}/mean_validation_rewards'.format(args.algo), mean_reward,
-                cum_steps)
+        if episode_cnt % config['test_log_interval'] == 0:
+            eval_rewards, eval_steps = run_evaluate_episodes(
+                agent, test_env, n_eval_episodes=5, render=False)
+            text_logger.info(
+                '[Eval], episode: {},  eval_rewards: {:.2f}'.format(
+                    episode_cnt, eval_rewards))
 
-            if args.use_wandb:
-                wandb.log({'test-reward': mean_reward})
-                wandb.log({'std_reward': std_reward})
+            test_results = {'env_step': eval_steps, 'rewards': eval_rewards}
+            logger.log_test_data(test_results, steps_cnt)
 
-    pbar.close()
+        progress_bar.update(episode_step)
+
     # render and record video
-    mean_reward, std_reward = evaluate(
+    run_evaluate_episodes(
         agent,
         test_env,
         n_eval_episodes=1,
